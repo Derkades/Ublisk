ade02670bbc5413c8a75581f2c0f8dbf7f053c44
RobinMC <robinslot@hotmail.nl>
2016-05-29 18:32:11 +0200

    Added prefix to other two messages

--- src/com/robinmc/ublisk/Messages.java
+++ src/com/robinmc/ublisk/Messages.java
@@ -5,11 +5,11 @@ import org.bukkit.ChatColor;
 public class Messages {
 	
 	public static String noPlayer(){
-		return ChatColor.RED + "You must be a player in order to execute this command!";
+		return ChatColor.GOLD + "Ublisk" + ChatColor.GRAY + " >> " + ChatColor.RED + "You must be a player in order to execute this command!";
 	}
 	
 	public static String songEnded(){
-		return ChatColor.GOLD + "Your music has ended. Please select a new song using /music";
+		return ChatColor.GOLD + "Music" + ChatColor.GRAY + " >> " + ChatColor.YELLOW + "MusicYour music has ended. Please select a new song using /music";
 	}
 	
 	public static String playerJoin(String pn){
