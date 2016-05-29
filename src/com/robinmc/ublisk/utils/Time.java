3fcd07b8ad97e98b799d86aff06569964b08303b
RobinMC <robinslot@hotmail.nl>
2016-05-29 14:58:08 +0200

    Time utils

---
+++ src/com/robinmc/ublisk/utils/Time.java
@@ -0,0 +1,30 @@
+package com.robinmc.ublisk.utils;
+
+import com.robinmc.ublisk.Var;
+
+	public class Time {
+	
+	public static boolean day() {
+	    long time = Var.world().getTime();	 
+	    if(time > 0 && time < 12300) {
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
+	
+	public static void set(long time){
+		Var.world().setTime(time);
+	}
+	
+	public static void add(long time){
+		long oldtime = get();
+		long newtime = oldtime + time;
+		Var.world().setTime(newtime);
+	}
+	
+	public static long get(){
+		return Var.world().getTime();
+	}
+
+}
