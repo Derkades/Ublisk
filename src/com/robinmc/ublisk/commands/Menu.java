a31b4abd14d404c0812b6a4da4a5fbef86eff8a1
Casper <casper.dennijs@gmail.com>
2016-05-29 14:13:33 +0200

    Basic Menu stuff

--- src/com/robinmc/ublisk/commands/Menu.java
+++ src/com/robinmc/ublisk/commands/Menu.java
@@ -1,5 +1,16 @@
 package com.robinmc.ublisk.commands;
 
-public class Menu {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+public class Menu implements CommandExecutor {
+	
+	@Override
+	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		  if (sender instanceof Player){
+			  Player player = (Player) sender;
+		  }
+	}
 }
