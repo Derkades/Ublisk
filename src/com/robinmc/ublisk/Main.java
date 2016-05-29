8afb5911e7fe0ccbf865f48e4c5e5c3a010c418e
Casper <casper.dennijs@gmail.com>
2016-05-29 13:56:16 +0200

    Main Class

--- src/com/robinmc/ublisk/Main.java
+++ src/com/robinmc/ublisk/Main.java
@@ -1,5 +1,16 @@
 package com.robinmc.ublisk;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+
+	}	
 }
