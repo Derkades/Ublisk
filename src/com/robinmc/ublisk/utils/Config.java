ba5d41c1c891b39237efdc09e952a8fc6a4d02f9
RobinMC <robinslot@hotmail.nl>
2016-05-29 13:57:31 +0200

    Added config utils

---
+++ src/com/robinmc/ublisk/utils/Config.java
@@ -0,0 +1,81 @@
+package com.robinmc.ublisk.utils;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import com.robinmc.minigames.Main;
+
+public class Config {
+	
+	static FileConfiguration config = Main.getInstance().getConfig();
+	
+	public static void create(){
+		try {
+			Main main = Main.getInstance(); //Get main's instance as I can't use "this" here
+			File file = new File(main.getDataFolder(), "config.yml"); //Set file variable
+			if (!main.getDataFolder().exists()) //Check if folder doesn't exist
+				main.getDataFolder().mkdirs(); //Then make one
+			if (!file.exists()){ //If file doesn't exist
+				main.getLogger().info("config.yml not found, creating...");
+				main.saveConfig(); //Make one
+			} else { //Otherwise just send message
+				main.getLogger().info("config.yml found, loading...");
+			}
+		} catch (Exception e){ //If everything fails
+			System.out.println("Critical error while creating a config!");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void reload(){
+		Main main = Main.getInstance();
+		main.reloadConfig(); //Reload the config. Pretty simple :D
+	}
+	
+	/**
+	 * Adds a string to the config file
+	 * @param path Path in config file
+	 * @param string String to add to config file
+	 */
+	public static void set(String path, String string){
+		config.set(path, string);
+		save();
+	}
+	
+	public static void set(String path, boolean state){
+		config.set(path, state);
+		save();
+	}
+	
+	public static void set(String path, int i){
+		config.set(path, i);
+		save();
+	}
+	
+	public static void set(String path, double n){
+		config.set(path, n);
+		save();
+	}
+	
+	public static void save(){
+		Main.getInstance().saveConfig();
+	}
+	
+	public static String getString(String path){
+		return config.getString(path);
+	}
+	
+	public static boolean getBoolean(String path){
+		return config.getBoolean(path);
+	}
+	
+	public static int getInteger(String path){
+		return config.getInt(path);
+	}
+	
+	public static double getDouble(String path){
+		return config.getDouble(path);
+	}
+
+}
