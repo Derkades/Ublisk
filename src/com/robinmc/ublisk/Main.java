68b12964d54f63f4a41764116c036c1b52abcd24
RobinMC <robinslot@hotmail.nl>
2016-05-29 13:59:19 +0200

    Added getInstance() method

--- src/com/robinmc/ublisk/Main.java
+++ src/com/robinmc/ublisk/Main.java
@@ -4,13 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	public static Main instance;
+	
 	@Override
 	public void onEnable(){
-		
+		instance = this;
 	}
 	
 	@Override
 	public void onDisable(){
-
-	}	
+		instance = null;
+	}
+	
+	public static Main getInstance(){
+		return instance;
+	}
 }
