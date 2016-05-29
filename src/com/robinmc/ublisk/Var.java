2642168fac98f8ef7d38ecf3a12458456b42222b
RobinMC <robinslot@hotmail.nl>
2016-05-29 14:58:14 +0200

    Variables class

---
+++ src/com/robinmc/ublisk/Var.java
@@ -0,0 +1,12 @@
+package com.robinmc.ublisk;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+public class Var {
+	
+	public static World world(){
+		return Bukkit.getWorld("world");
+	}
+
+}
