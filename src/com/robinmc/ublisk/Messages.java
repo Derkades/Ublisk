1646dd0015f60129e5fa6a4b9f2f45fcf462c791
RobinMC <robinslot@hotmail.nl>
2016-05-29 16:33:13 +0200

    Song end event

--- src/com/robinmc/ublisk/Messages.java
+++ src/com/robinmc/ublisk/Messages.java
@@ -7,5 +7,9 @@ public class Messages {
 	public static String noPlayer(){
 		return ChatColor.RED + "You must be a player in order to execute this command!";
 	}
+	
+	public static String songEnded(){
+		return ChatColor.GOLD + "Your music has ended. Please select a new song using /music";
+	}
 
 }
---
+++ src/com/robinmc/ublisk/listeners/SongEnd.java
@@ -0,0 +1,17 @@
+package com.robinmc.ublisk.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import com.robinmc.ublisk.Messages;
+import com.xxmicloxx.NoteBlockAPI.SongEndEvent;
+
+public class SongEnd implements Listener {
+	
+	@EventHandler
+	public void musicStopped(SongEndEvent event){
+		Player player = (Player) event.getSongPlayer();
+		player.sendMessage(Messages.songEnded());
+	}
+}
