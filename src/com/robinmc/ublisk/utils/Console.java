53250e727fe5ea0b8ae7818f4dd4e49289279a0e
RobinMC <robinslot@hotmail.nl>
2016-05-29 13:53:47 +0200

    Added console utils

---
+++ src/com/robinmc/ublisk/utils/Console.java
@@ -0,0 +1,32 @@
+package com.robinmc.ublisk.utils;
+
+import org.bukkit.Bukkit;
+
+public class Console {
+	
+	/**
+	 * Sends a message to the console
+	 * @param msg Message to be sent
+	 */
+	public static void sendMessage(String msg){
+		try {
+			Bukkit.getServer().getConsoleSender().sendMessage(msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	/**
+	 * Execute a command as the console (without the /)
+	 * @param cmd The command to be executed
+	 */
+	public static void sendCommand(String cmd){
+		try {
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
+		} catch (Exception e){
+			sendMessage("An error occured while attempting to perform a console command!");
+			sendMessage("Command: " + cmd);
+		}
+	}
+
+}
