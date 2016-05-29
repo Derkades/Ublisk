e9b1e2c0818227a7349aa6b83a7018cd052e743a
RobinMC <robinslot@hotmail.nl>
2016-05-29 14:16:54 +0200

    Finished /menu

--- src/com/robinmc/ublisk/commands/Menu.java
+++ src/com/robinmc/ublisk/commands/Menu.java
@@ -5,12 +5,19 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.robinmc.ublisk.iconmenus.MainMenu;
+
 public class Menu implements CommandExecutor {
 	
 	@Override
-	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		  if (sender instanceof Player){
-			  Player player = (Player) sender;
-		  }
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player){
+			Player player = (Player) sender;
+			MainMenu.open(player); //Open menu
+			return true;
+		} else {
+			//TODO: No player msg
+			return true;
+		}
 	}
 }
