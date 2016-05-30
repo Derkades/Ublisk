9f7c20c803cb7a3d114c018973cfd5bd37229ded
RobinMC <robinslot@hotmail.nl>
2016-05-30 20:40:46 +0200

    Why didn't eclipse save this?

--- src/com/robinmc/ublisk/commands/Music.java
+++ src/com/robinmc/ublisk/commands/Music.java
@@ -6,7 +6,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.robinmc.ublisk.Messages;
+import com.robinmc.ublisk.iconmenus.MusicMenu;
+
+public class Music {
+	
+	public class Menu implements CommandExecutor {
+		
+		@Override
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			if (sender instanceof Player){
 				Player player = (Player) sender;
+				MusicMenu.open(player);
 				return true;
 			} else {
 				sender.sendMessage(Messages.noPlayer());
