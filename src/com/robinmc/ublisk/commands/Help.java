1db009aa394293730e78b379e9b5e1db4e79c88f
RobinMC <robinslot@hotmail.nl>
2016-06-02 19:47:10 +0200

    Updated /help

--- src/com/robinmc/ublisk/commands/Help.java
+++ src/com/robinmc/ublisk/commands/Help.java
@@ -19,6 +19,39 @@ public class Help implements CommandExecutor {
 			sender.sendMessage("");
 			sender.sendMessage(ChatColor.GRAY + "Type /help [number] for help regarding that catagory");
 			return true;
+		} else if (args.length == 1){
+			if (args[0].equals("1") || args[0].equalsIgnoreCase("faq")){
+				sender.sendMessage("Ublisk faq menu - Please choose a catagory");
+				sender.sendMessage("");
+				sender.sendMessage(ChatColor.GOLD + "1" + ChatColor.GRAY + ")" + ChatColor.YELLOW + " General");
+				sender.sendMessage(ChatColor.GOLD + "2" + ChatColor.GRAY + ")" + ChatColor.YELLOW + " Plugins");
+				sender.sendMessage("");
+				sender.sendMessage(ChatColor.GRAY + "Now type /help faq [number]");
+				return true;
+			} else if (args[0].equals("2") || args[0].equalsIgnoreCase("commands")){
+				sender.sendMessage("Nothing here yet!");
+				return true;
+			} else {
+				sender.sendMessage(Messages.wrongUsage());
+				return true;
+			}
+		} else if (args.length == 2){
+			if (args[0].equalsIgnoreCase("faq")){
+				if (args[1].equals("1")){
+					sender.sendMessage("Nothing here yet!");
+					return true;
+				} else if (args[1].equals("2")){
+					sender.sendMessage(ChatColor.GOLD + "Q: Does this server use custom plugins?");
+					sender.sendMessage(ChatColor.YELLOW + "A: Yes. Everything is custom coded except for API plugins. To see a full list of all plugins our server uses type /plugins");
+					return true;
+				} else {
+					sender.sendMessage(Messages.wrongUsage());
+					return true;
+				}
+			} else {
+				sender.sendMessage(Messages.wrongUsage());
+				return true;
+			}
 		} else {
 			sender.sendMessage(Messages.wrongUsage());
 			return true;
