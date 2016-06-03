7e57852291b370bbef4fb797a317f73b01482ca7
RobinMC <robinslot@hotmail.nl>
2016-06-03 22:06:46 +0200

    Messages cleanup

--- src/com/robinmc/ublisk/Messages.java
+++ src/com/robinmc/ublisk/Messages.java
@@ -9,49 +9,41 @@ public class Messages {
 	static ChatColor red = ChatColor.RED;
 	static ChatColor yellow = ChatColor.YELLOW;
 	
+	//----------------------------- Join and quit -----------------------------//
+	
+	public static String playerJoin(String pn){
+		return gold + "Ublisk" + gray + " >> " + yellow + pn + " has joined";
+	}	
+	
+	public static String quit(String pn){
+		return gold + "Ublisk" + gray + " >> " + yellow + pn + "has left";
+	}
+	
+	//----------------------------- Commands -----------------------------//
+	
 	public static String noPlayer(){
 		return gold + "Ublisk" + gray + " >> " + red + "You must be a player in order to execute this command!";
 	}
 	
+	public static String wrongUsage(){
+		return gold + "Ublisk" + gray + " >> " + red + "Wrong usage! Type /help for help";
+	}
+		
+	public static String userNotFound(String user){
+		return gold + "Ublisk" + gray + " >> " + red + "No additional information was found for user " + user;
+	}
+	
+	//----------------------------- Music -----------------------------//
+	
 	public static String songEnded(){
 		return gold + "Music" + gray + " >> " + yellow + "Your song has ended. Please select a new song using /music";
-	}
-	
-	public static String playerJoin(String pn){
-		return gold + "Ublisk" + gray + " >> " + yellow + pn + " has joined";
-	}
-	
-	public static String menuErrorWrongItem(){
-		return gold + "Menu" + gray + " >> " + red + "An unexpected error has occured. Please report this error and the steps you took to get this error at the forums.";
-	}
-	
-	public static String cantEat(String pn){
-		return gold + "Ublisk" + gray + " >> " + red + pn + " Eating food is not allowed on this server";
 	}
 	
 	public static String startSong(String song){
 		return gold + "Music" + gray + " >> " + yellow + "You are now playing " + song;
 	}
 	
-	public static String wrongUsage(){
-		return gold + "Ublisk" + gray + " >> " + red + "Wrong usage! Type /help for help";
-	}
-	
-	public static String tntDetect(){
-		return gold + "Ublisk" + gray + " >> " + red + "TNT has been detected in your inventory. Using it will result in a 30s temp-ban";
-	}
-	
-	public static String tntBan(){
-		return red + "You have been banned for using TNT. You will be unbanned automatically in 30 seconds.";
-	}
-	
-	public static String removeMobsWarning(int sec){
-		return gold + "Ublisk" + gray + " >> " + yellow + "Clearing all mobs and items in " + sec + " seconds!";
-	}
-	
-	public static String removedMobs(){
-		return gold + "Ublisk" + gray + " >> " + yellow + "All mobs and items have been cleared!";
-	}
+	//----------------------------- Resource pack -----------------------------//
 	
 	public static String declinedPack(){
 		return red + "Please enable server resource packs and join again";
@@ -69,12 +61,30 @@ public class Messages {
 		return gold + "Ublisk" + gray + " >> " + yellow + "Sending you our resource pack...";
 	}
 	
-	public static String quit(String pn){
-		return gold + "Ublisk" + gray + " >> " + yellow + pn + "has left";
+	//----------------------------- Miscellaneous -----------------------------//
+	
+	public static String menuErrorWrongItem(){
+		return gold + "Menu" + gray + " >> " + red + "An unexpected error has occured. Please report this error and the steps you took to get this error at the forums.";
 	}
 	
-	public static String userNotFound(String user){
-		return gold + "Ublisk" + gray + " >> " + red + "No additional information was found for user " + user;
+	public static String cantEat(String pn){
+		return gold + "Ublisk" + gray + " >> " + red + pn + " Eating food is not allowed on this server";
+	}
+	
+	public static String tntDetect(){
+		return gold + "Ublisk" + gray + " >> " + red + "TNT has been detected in your inventory. Using it will result in a 30s temp-ban";
+	}
+	
+	public static String tntBan(){
+		return red + "You have been banned for using TNT. You will be unbanned automatically in 30 seconds.";
+	}
+	
+	public static String removeMobsWarning(int sec){
+		return gold + "Ublisk" + gray + " >> " + yellow + "Clearing all mobs and items in " + sec + " seconds!";
+	}
+	
+	public static String removedMobs(){
+		return gold + "Ublisk" + gray + " >> " + yellow + "All mobs and items have been cleared!";
 	}
 	
 }
