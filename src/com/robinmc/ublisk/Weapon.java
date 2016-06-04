4dd4089634bb98ba9330b7448287d8fa99d80d0e
Jerrijn <jerrijn@hotmail.com>
2016-06-04 11:41:32 +0200

    Added Basic Sword

--- src/com/robinmc/ublisk/Weapon.java
+++ src/com/robinmc/ublisk/Weapon.java
@@ -12,7 +12,8 @@ public enum Weapon {
 	//Put a \ before every "
 	OLD_WOODEN_SWORD("minecraft:wooden_sword 1 0 {HideFlags:2,display:{Name:\"Old Wooden Sword\",Lore:[\"Attack Damage: 1\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:1,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
 	OLD_STONE_SWORD("minecraft:stone_sword 1 0 {HideFlags:2,display:{Name:\"Old Stone Sword\",Lore:[\"Attack Damage: 2\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:2,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
-	OLD_IRON_SWORD("minecraft:iron_sword 1 0 {HideFlags:2,display:{Name:\"Old Iron Sword\",Lore:[\"Attack Damage: 3\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:3,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}");
+	OLD_IRON_SWORD("minecraft:iron_sword 1 0 {HideFlags:2,display:{Name:\"Old Iron Sword\",Lore:[\"Attack Damage: 3\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:3,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
+	WOODEN_SWORD("minecraft:wooden_sword 1 0 {Unbreakable:1,display:{Name:\"Basic Sword\",Lore:[\"This weapons deals 10 extra magic damage\"]},AttributeModifiers:[{AttributeName:\"generic.movementSpeed\",Name:\"generic.movementSpeed\",Amount:10,Operation:1,UUIDMost:45995,UUIDLeast:100431}]}");
 	
 	private String cmd;
 	
