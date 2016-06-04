74d87d0f863e5814b966d07c3b5b4f74b62e2bf7
RobinMC <robinslot@hotmail.nl>
2016-06-04 10:10:42 +0200

    Cleanup

--- src/com/robinmc/ublisk/commands/Help.java
+++ src/com/robinmc/ublisk/commands/Help.java
@@ -5,80 +5,43 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.robinmc.ublisk.HelpMessages;
 import com.robinmc.ublisk.Messages;
 
 public class Help implements CommandExecutor {
 	
-	ChatColor gold = ChatColor.GOLD;
-	ChatColor gray = ChatColor.GRAY;
-	ChatColor yellow = ChatColor.YELLOW;
+	HelpMessages msg = new HelpMessages();
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if (args.length == 0){
-			sender.sendMessage("");
-			sender.sendMessage(gold + "Ublisk help menu - Please choose a catagory");
-			sender.sendMessage("");
-			sender.sendMessage(gold + "1" + gray + ")" + yellow + " FAQ");
-			sender.sendMessage(gold + "2" + gray + ")" + yellow + " Commands");
-			sender.sendMessage("");
-			sender.sendMessage(gray + "Type /help [number] for help regarding that catagory");
-			return true;
+			return msg.main(sender);
 		} else if (args.length == 1){
 			if (args[0].equals("1") || args[0].equalsIgnoreCase("faq")){
-				sender.sendMessage("");
-				sender.sendMessage(gold + "Ublisk faq menu - Please choose a catagory");
-				sender.sendMessage("");
-				sender.sendMessage(gold + "1" + gray + ")" + yellow + " General");
-				sender.sendMessage(gold + "2" + gray + ")" + yellow + " Plugins");
-				sender.sendMessage("");
-				sender.sendMessage(gray + "Now type /help faq [number]");
-				return true;
+				return msg.faq(sender);
 			} else if (args[0].equals("2") || args[0].equalsIgnoreCase("commands")){
-				sender.sendMessage("");
-				sender.sendMessage(gold + "List of all commands - type /help command [command name or number]");
-				sender.sendMessage("");
-				sender.sendMessage(gold + "1" + gray + ")" + yellow + " Credits");
-				sender.sendMessage(gold + "2" + gray + ")" + yellow + " Menu");
-				sender.sendMessage(gold + "3" + gray + ")" + yellow + " Music");
-				return true;
+				return msg.commands(sender);
 			} else {
 				sender.sendMessage(Messages.wrongUsage());
 				return true;
 			}
 		} else if (args.length == 2){
 			if (args[0].equalsIgnoreCase("faq")){
-				if (args[1].equals("1")){
-					sender.sendMessage("");
-					sender.sendMessage(gold + "Q: How can I get wood?");
-					sender.sendMessage(yellow + "A: You can get wood by clicking the lever at the right side of the saw at 80 -75");
-					return true;
-				} else if (args[1].equals("2")){
-					sender.sendMessage("");
-					sender.sendMessage(gold + "Q: Does this server use custom plugins?");
-					sender.sendMessage(yellow + "A: Yes. Everything is custom coded except for API plugins. To see a full list of all plugins our server uses type /plugins");
-					return true;
+				if (args[1].equals("1") || args[1].equalsIgnoreCase("general")){
+					return msg.faqGeneral(sender);
+				} else if (args[1].equals("2") || args[1].equalsIgnoreCase("commands")){
+					return msg.faqPlugins(sender);
 				} else {
 					sender.sendMessage(Messages.wrongUsage());
 					return true;
 				}
 			} else if (args[0].equalsIgnoreCase("command") || args[0].equals("2") || args[0].equalsIgnoreCase("command")){
 				if (args[1].equals("1") || args[1].equalsIgnoreCase("credits")){
-					sender.sendMessage("");
-					sender.sendMessage(gold + "Description" + gray + ":" + yellow + " This command shows who has made what on this server");
-					sender.sendMessage(gold + "Usage" + gray + ":");
-					sender.sendMessage(yellow + "/credits" + gray + " - To give a list of who did what");
-					sender.sendMessage(yellow + "/credits [username]" + gray + " - To get more information about a specific user");
-					sender.sendMessage(yellow + "/credits map" + gray + " - To view who built what");
-					return true;
+					return msg.commandsCredits(sender);
 				} else if (args[1].equals("2") || args[1].equalsIgnoreCase("menu")){
-					sender.sendMessage("");
-					sender.sendMessage(gray + "Coming soon!");
-					return true;
+					return msg.commandsMenu(sender);
 				} else if (args[1].equals("3") || args[1].equalsIgnoreCase("music")){
-					sender.sendMessage("");
-					sender.sendMessage(gray + "Coming soon!");
-					return true;
+					return msg.commandsMusic(sender);
 				} else {
 					sender.sendMessage(ChatColor.RED + "Unknown command");
 					return true;
