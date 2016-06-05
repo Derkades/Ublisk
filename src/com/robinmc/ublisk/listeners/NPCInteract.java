dfc92833cd6002a57e11c6d0abeb133a6fd43c3e
RobinMC <robinslot@hotmail.nl>
2016-06-05 22:12:12 +0200

    NPC click cooldowns

--- src/com/robinmc/ublisk/listeners/NPCInteract.java
+++ src/com/robinmc/ublisk/listeners/NPCInteract.java
@@ -1,9 +1,15 @@
 package com.robinmc.ublisk.listeners;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import com.robinmc.ublisk.HashMaps;
+import com.robinmc.ublisk.Main;
+import com.robinmc.ublisk.npc.Merek;
 import com.robinmc.ublisk.utils.Console;
 
 import de.inventivegames.npc.event.NPCInteractEvent;
@@ -13,12 +19,28 @@ public class NPCInteract implements Listener {
 	
 	@EventHandler
 	public void npcInteract(NPCInteractEvent event){
-		InteractType type = event.getType();
-		if (type == InteractType.RIGHT_CLICK){
-			Player player = event.getPlayer();
-			String name = event.getNPC().getName();
-			Console.sendMessage("[NPC] " + player.getName() + " right clicked npc " + name);
-			player.sendMessage("Hi there!");
+		Player player = event.getPlayer();
+		final UUID uuid = player.getUniqueId();
+		Console.sendMessage("Trigger!");
+		if (!HashMaps.cooldownnpc.get(uuid)){
+			Console.sendMessage("Cooldown!");
+			InteractType type = event.getType();
+			if (type == InteractType.RIGHT_CLICK){
+				HashMaps.cooldownnpc.put(uuid, true);
+				String name = event.getNPC().getName();
+				Console.sendMessage("[NPC] " + player.getName() + " right clicked npc " + name);
+				
+				Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable(){
+					public void run(){
+						HashMaps.cooldownnpc.put(uuid, false);
+					}
+				}, 5*20);
+				
+				if (name == "Merek"){
+					Merek.merek(player);
+				}
+				
+			}
 		}
 	}
 
