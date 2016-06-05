93f7bcdc93908b6dd46cd7a925b6341f249c1bb5
RobinMC <robinslot@hotmail.nl>
2016-06-05 22:12:33 +0200

    Cooldown hashmap

--- src/com/robinmc/ublisk/HashMaps.java
+++ src/com/robinmc/ublisk/HashMaps.java
@@ -12,6 +12,7 @@ import com.robinmc.ublisk.utils.Console;
 public class HashMaps {
 
 	public static Map<UUID, Boolean> afk = new HashMap<UUID, Boolean>();
+	public static Map<UUID, Boolean> cooldownnpc = new HashMap<UUID, Boolean>();
 	
 	static void resetAllPlayers(){
 		for (Player p: Bukkit.getOnlinePlayers()) HashMaps.addPlayerToMaps(p);
@@ -19,7 +20,9 @@ public class HashMaps {
 	
 	public static void addPlayerToMaps(Player player){
 		Console.sendMessage("[HashMaps] " + player.getName() + "'s maps has been reset");
-		afk.put(player.getUniqueId(), false);
+		UUID uuid = player.getUniqueId();
+		afk.put(uuid, false);
+		cooldownnpc.put(uuid, false);
 	}
 	
 }
