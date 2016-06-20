c61373497ebc054a6aa52ea8003fd9eef4b7b52a
RobinMC <robinslot@hotmail.nl>
2016-06-20 20:09:11 +0200

    Added NPC Ulric

--- src/com/robinmc/ublisk/NPCs.java
+++ src/com/robinmc/ublisk/NPCs.java
@@ -9,7 +9,8 @@ public class NPCs {
 	
 	public static void spawnAll(){
 		NPCUtils npc = new NPCUtils();
-		npc.spawnNPC(33, 67, -38, -70, 0, "Merek", "Lunrs");
+		npc.spawnNPC(33, 67, -38, -70, 0, "Merek", "Lunrpulse");
+		npc.spawnNPC(38.5, 67, -26.5, -145, 0, "Ulric", "Chaspyr");
 	}
 	
 	public static void despawnAll(){
