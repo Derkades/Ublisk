0bfaad05e5a722fe3f41a63ab17438437af26add
RobinMC <robinslot@hotmail.nl>
2016-06-20 17:04:41 +0200

    SQL stuff

--- src/com/robinmc/ublisk/Main.java
+++ src/com/robinmc/ublisk/Main.java
@@ -1,5 +1,12 @@
 package com.robinmc.ublisk;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.robinmc.ublisk.utils.Commands;
@@ -9,6 +16,8 @@ import com.robinmc.ublisk.utils.Listeners;
 public class Main extends JavaPlugin {
 	
 	public static Main instance;
+	
+	public static Connection connection;
 	
 	@Override
 	public void onEnable(){
@@ -29,6 +38,46 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable(){
 		instance = null;
+		try {
+			if (connection != null && connection.isClosed()){
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized static void openConnection(){
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://192.168.0.125:3306/ublisk", "ublisk", "UJpwZBuEpw5C8MUv");
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized static void closeConnection(){
+		try {
+			connection.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized static boolean playerDataContainsPlayer(Player player){
+		try {
+			PreparedStatement sql = connection.prepareStatement("SELECT * FROM `login_count` WHERE uuid=?;");
+			sql.setString(1, player.getUniqueId().toString());
+			ResultSet resultSet = sql.executeQuery();
+			boolean containsPlayer = resultSet.next();
+			
+			sql.close();
+			resultSet.close();
+			
+			return containsPlayer;
+		} catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 	public static Main getInstance(){
