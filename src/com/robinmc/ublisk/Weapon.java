e9e6fb5bdfce9319e81ef6356f1fd16972a0d6f1
RobinMC <robinslot@hotmail.nl>
2016-06-26 10:53:31 +0200

    Class in weapon constant

--- src/com/robinmc/ublisk/Weapon.java
+++ src/com/robinmc/ublisk/Weapon.java
@@ -10,22 +10,24 @@ public enum Weapon {
 	//Format: "minecraft:stone 1 0 {AttributeModifiers:[{Attrib...IDLeast:138836}]}"
 	//So, remove the /give @p
 	//Put a \ before every "
-	OLD_WOODEN_SWORD("minecraft:wooden_sword 1 0 {HideFlags:2,display:{Name:\"Old Wooden Sword\",Lore:[\"Attack Damage: 1\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:1,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
-	OLD_STONE_SWORD("minecraft:stone_sword 1 0 {HideFlags:2,display:{Name:\"Old Stone Sword\",Lore:[\"Attack Damage: 2\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:2,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
-	OLD_IRON_SWORD("minecraft:iron_sword 1 0 {HideFlags:2,display:{Name:\"Old Iron Sword\",Lore:[\"Attack Damage: 3\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:3,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}");
+	OLD_WOODEN_SWORD(Classes.SWORDSMAN, "minecraft:wooden_sword 1 0 {HideFlags:2,display:{Name:\"Old Wooden Sword\",Lore:[\"Attack Damage: 1\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:1,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
+	OLD_STONE_SWORD(Classes.SWORDSMAN, "minecraft:stone_sword 1 0 {HideFlags:2,display:{Name:\"Old Stone Sword\",Lore:[\"Attack Damage: 2\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:2,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}"),
+	OLD_IRON_SWORD(Classes.SWORDSMAN, "minecraft:iron_sword 1 0 {HideFlags:2,display:{Name:\"Old Iron Sword\",Lore:[\"Attack Damage: 3\"]},AttributeModifiers:[{AttributeName:\"generic.attackDamage\",Name:\"generic.attackDamage\",Amount:3,Operation:0,UUIDMost:64262,UUIDLeast:178260}]}");
 	
+	private Classes c;
 	private String cmd;
 	
-	Weapon(String cmd){
+	Weapon(Classes c, String cmd){
+		this.c = c;
 		this.cmd = cmd;
+	}
+	
+	public Classes getClazz(){
+		return c;
 	}
 	
 	public String getCmd(){
 		return cmd;
-	}
-	
-	public void setCmd(String cmd){
-		this.cmd = cmd;
 	}
 	
 	public static void giveWeapon(Player player, Weapon weapon){
