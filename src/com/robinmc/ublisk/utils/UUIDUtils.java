665bf23b4c70fa40dc8aeefd56d115602d922fcb
RobinMC <robinslot@hotmail.nl>
2016-07-10 16:45:01 +0200

    Added new methods

--- src/com/robinmc/ublisk/utils/UUIDUtils.java
+++ src/com/robinmc/ublisk/utils/UUIDUtils.java
@@ -2,6 +2,9 @@ package com.robinmc.ublisk.utils;
 
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
 public class UUIDUtils {
 	
 	public static UUID getIdFromName(String name){
@@ -12,6 +15,24 @@ public class UUIDUtils {
 	public static String getNameFromId(UUID uuid){
 		String name = Config.getString("uuid.name." + uuid);
 		return name;
+	}
+	
+	public static UUID fromString(String string){
+		UUID uuid = UUID.fromString(string);
+		return uuid;
+	}
+	
+	public static String getNameFromIdString(String string){
+		String name = getNameFromId(fromString(string));
+		return name;
+	}
+	
+	public static OfflinePlayer getPlayerFromId(UUID uuid){
+		return Bukkit.getOfflinePlayer(uuid);
+	}
+	
+	public static OfflinePlayer getPlayerFromName(String name){
+		return Bukkit.getOfflinePlayer(getIdFromName(name));
 	}
 
 }
