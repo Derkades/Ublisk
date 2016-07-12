5ff55de9685fda39da467cc4a93ec8e6d315b875
RobinMC <robinslot@hotmail.nl>
2016-07-12 16:55:28 +0200

    Cleanup / Use quests api instead

--- src/com/robinmc/ublisk/npc/Merek.java
+++ src/com/robinmc/ublisk/npc/Merek.java
@@ -2,32 +2,30 @@ package com.robinmc.ublisk.npc;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 import com.robinmc.ublisk.utils.NPCUtils;
+import com.robinmc.ublisk.utils.Quests;
 
 public class Merek {
 	
 	public static void merek(Player player){
-		NPCUtils npc = new NPCUtils();
-		PlayerInventory inv = player.getInventory();
-		
-		if (	inv.containsAtLeast(new ItemStack(Material.LOG), 10) &&
-				inv.containsAtLeast(new ItemStack(Material.STRING), 10) &&
-				inv.containsAtLeast(new ItemStack(Material.GOLD_NUGGET), 10)){
+		NPCUtils npc = Quests.getNPCApi();
+		if (Quests.playerHasItem(player, Material.LOG, 10) 
+				&& Quests.playerHasItem(player, Material.STRING, 16)
+				&& Quests.playerHasItem(player, Material.GOLD_NUGGET, 10)){
 			npc.msg(player, "Merek", "Great! Now go to Ulric to craft a weapon.");
-		} else if (inv.containsAtLeast(new ItemStack(Material.STRING), 16) && inv.containsAtLeast(new ItemStack(Material.GOLD_NUGGET), 10)){
+		} else if (Quests.playerHasItem(player, Material.STRING, 16)
+				&& Quests.playerHasItem(player, Material.GOLD_NUGGET, 10)){
 			npc.msg(player, "Merek", "Finally get 10 wood logs at the saw");
-		} else if (inv.containsAtLeast(new ItemStack(Material.WOOL), 4) && inv.containsAtLeast(new ItemStack(Material.GOLD_NUGGET), 10)){
+		} else if (Quests.playerHasItem(player, Material.WOOL, 4)
+				&& Quests.playerHasItem(player, Material.GOLD_NUGGET, 10)){
 			npc.msg(player, "Merek", "Great job, now please break down your wool into 16 string by using the windmill.");
-		} else if (inv.containsAtLeast(new ItemStack(Material.GOLD_NUGGET), 10)){
+		} else if (Quests.playerHasItem(player, Material.GOLD_NUGGET)){
 			npc.msg(player, "Merek", "Now bring me 4 wool by killing sheep.");
 		} else {
 			npc.msg(player, "Merek", "What are you doing out here? You don’t even have a weapon yet! I’ll tell you what you’ll need to make a weapon. First, get 10 golden nuggets by killing animals.");
 		}
 			
-		
 	}
 
 }
