a35611eb7d9625db6adb5a9853119b81e57345df
RobinMC <robinslot@hotmail.nl>
2016-07-12 13:36:13 +0200

    Quests api

---
+++ src/com/robinmc/ublisk/utils/Quests.java
@@ -0,0 +1,61 @@
+package com.robinmc.ublisk.utils;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import com.robinmc.ublisk.Quest;
+
+public class Quests {
+	
+	public static void saveProgress(Player player, Quest quest, String data){
+		Config.set("quests." + player.getUniqueId() + "." + quest.getName(), true);
+	}
+	
+	public static boolean getProgress(Player player, Quest quest, String data){
+		try {
+			return Config.getBoolean("quests." + quest.getName());
+		} catch (Exception e){
+			return false;
+		}
+	}
+	
+	public static void setQuestCompleted(Player player, Quest quest, boolean bool){
+		Config.set("quest." + player.getUniqueId() + "." + quest.getName(), bool);
+	}
+	
+	public static boolean getQuestCompleted(Player player, Quest quest){
+		return Config.getBoolean("quests." + player.getUniqueId() + "." + quest.getName());
+	}
+	
+	public static boolean playerHasItem(Player player, Material material, int amount){
+		PlayerInventory inv = player.getInventory();
+		return inv.containsAtLeast(new ItemStack(material), amount);
+	}
+	
+	public static boolean playerHasItem(Player player, Material material){
+		return playerHasItem(player, material, 1);
+	}
+	
+	public static int getLevel(Quest quest, int level){
+		return quest.getLevel();
+	}
+	
+	public static String getName(Quest quest){
+		return quest.getName();
+	}
+	
+	public static void removeItems(Player player, Material material, int amount){
+		player.getInventory().remove(new ItemStack(material, amount));
+	}
+	
+	public static void removeItem(Player player, Material material){
+		removeItems(player, material, 1);
+	}
+	
+	public static NPCUtils getNPCApi(){
+		return new NPCUtils();
+	}
+	
+}
