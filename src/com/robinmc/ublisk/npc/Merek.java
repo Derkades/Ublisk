a8deec3c34f6a1306ec73b5962553ac27a22fa1a
RobinMC <robinslot@hotmail.nl>
2016-07-14 11:58:28 +0200

    Updated Merek code with new Quest api

--- src/com/robinmc/ublisk/npc/Merek.java
+++ src/com/robinmc/ublisk/npc/Merek.java
@@ -2,6 +2,7 @@ package com.robinmc.ublisk.npc;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import com.robinmc.ublisk.utils.NPCUtils;
 import com.robinmc.ublisk.utils.Quest;	
@@ -10,15 +11,18 @@ public class Merek {
 	
 	public static void merek(Player player){
 		NPCUtils npc = Quest.getNPCApi();
-		if (Quest.playerHasItem(player, Material.LOG, 10) 
-				&& Quest.playerHasItem(player, Material.STRING, 16)
-				&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		//if (Quest.playerHasItem(player, Material.LOG, 10) 
+		//		&& Quest.playerHasItem(player, Material.STRING, 16)
+		//		&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		if (Quest.playerHasItems(player, new ItemStack(Material.LOG, 10), new ItemStack(Material.STRING, 16), new ItemStack(Material.GOLD_NUGGET, 10))){
 			npc.msg(player, "Merek", "Great! Now go to Ulric to craft a weapon.");
-		} else if (Quest.playerHasItem(player, Material.STRING, 16)
-				&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		//} else if (Quest.playerHasItem(player, Material.STRING, 16)
+		//		&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		} else if (Quest.playerHasItems(player, new ItemStack(Material.STRING, 16), new ItemStack(Material.GOLD_NUGGET, 10))){
 			npc.msg(player, "Merek", "Finally get 10 wood logs at the saw");
-		} else if (Quest.playerHasItem(player, Material.WOOL, 4)
-				&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		//} else if (Quest.playerHasItem(player, Material.WOOL, 4)
+		//		&& Quest.playerHasItem(player, Material.GOLD_NUGGET, 10)){
+		} else if (Quest.playerHasItems(player, new ItemStack(Material.WOOL, 4), new ItemStack(Material.GOLD_NUGGET, 10))){
 			npc.msg(player, "Merek", "Great job, now please break down your wool into 16 string by using the windmill.");
 		} else if (Quest.playerHasItem(player, Material.GOLD_NUGGET)){
 			npc.msg(player, "Merek", "Now bring me 4 wool by killing sheep.");
