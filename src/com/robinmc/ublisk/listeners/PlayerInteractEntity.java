a0c3a0de7d38ec12706a353ab607807897b02893
RobinMC <robinslot@hotmail.nl>
2016-07-14 14:52:02 +0200

    Removed unused import

--- src/com/robinmc/ublisk/listeners/PlayerInteractEntity.java
+++ src/com/robinmc/ublisk/listeners/PlayerInteractEntity.java
@@ -3,7 +3,6 @@ package com.robinmc.ublisk.listeners;
 import org.bukkit.GameMode;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.NPC;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,11 +14,12 @@ public class PlayerInteractEntity implements Listener {
 	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
 		Entity entity = event.getRightClicked();
 		Player player = event.getPlayer();
+		/*
 		if (entity instanceof NPC){
 			String name = entity.getName();
 			WeaponMerchant.open(name, player);
 			event.setCancelled(true);
-		/*
+		} else */ if (entity instanceof ArmorStand && !(player.getGameMode() == GameMode.CREATIVE)){
 			event.setCancelled(true);
 		}
 
