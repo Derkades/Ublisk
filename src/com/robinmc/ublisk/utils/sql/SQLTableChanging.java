6857366f27ad851ae01aea74fb393104dd4f8f75
RobinMC <robinslot@hotmail.nl>
2016-08-08 14:44:43 +0200

    Fixed throwing ConnectionClosedExceptions

--- src/com/robinmc/ublisk/utils/sql/SQLTableChanging.java
+++ src/com/robinmc/ublisk/utils/sql/SQLTableChanging.java
@@ -1,6 +1,5 @@
 package com.robinmc.ublisk.utils.sql;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -9,12 +8,6 @@ import com.robinmc.ublisk.utils.exception.ConnectionClosedException;
 public class SQLTableChanging {
 	
 	public static PreparedStatement prepareStatement(String sql) throws ConnectionClosedException, SQLException {
-		Connection con = MySQL.connection;
-		
-		if (con == null || con.isClosed()){
-			throw new ConnectionClosedException();
-		}
-		
 		return MySQL.connection.prepareStatement(sql);
 		
 	}
