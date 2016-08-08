503c2c0309ace17714231b5ef700f8b35ab51b6c
RobinMC <robinslot@hotmail.nl>
2016-08-08 21:54:28 +0200

    Cleanup

--- src/com/robinmc/ublisk/npc/Dianh.java
+++ src/com/robinmc/ublisk/npc/Dianh.java
@@ -14,19 +14,16 @@ public class Dianh implements QuestCharacterClass {
 
 	@Override
 	public void talk(Player player) {
-		// TODO Auto-generated method stub
-		QuestParticipant searchMeat = new QuestParticipant(player, Quest.SEARCH_MEAT, QuestCharacter.DIANH);
-		BetterInventory inv = searchMeat.getInventory();
+		QuestParticipant qp = new QuestParticipant(player, Quest.SEARCH_MEAT, QuestCharacter.DIANH);
+		BetterInventory inv = qp.getInventory();
 		
-		if(searchMeat.getProgress(QuestProgress.SEARCH_FOR_MEAT_TALK_TO_ARZHUR) && inv.contains(Material.ROTTEN_FLESH, 20))
-		{
-			searchMeat.msg("Hi there! I am Dianh, do you have anything for me to purify? I do this daily, people come up to me and want me to purify water and food to make sure it is healthy. Also you can turn your items back into 50% of the costs such as weapons and armor or in this case food! Well anyway, let’s start the purification.");
+		if(qp.getProgress(QuestProgress.SEARCH_FOR_MEAT_TALK_TO_ARZHUR) && inv.contains(Material.ROTTEN_FLESH, 20)){
+			qp.msg("Hi there! I am Dianh, do you have anything for me to purify? I do this daily, people come up to me and want me to purify water and food to make sure it is healthy. Also you can turn your items back into 50% of the costs such as weapons and armor or in this case food! Well anyway, let’s start the purification.");
 			inv.remove(Material.ROTTEN_FLESH, 20);
 			inv.add(Material.GRILLED_PORK, 10);
 		} else {
-			searchMeat.msg("Hello dear, what ya looking at?!");
+			qp.msg("Hello dear, what ya looking at?!");
 		}
-		
 	}
 
 }
