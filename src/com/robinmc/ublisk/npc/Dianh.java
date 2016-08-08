03bbe3e0302df6eb0fc5b57f1c488e99aa5cfa76
Sanderje2000 <sander.engelhart@gmail.com>
2016-08-08 13:32:31 +0200

    Added Diahn class

---
+++ src/com/robinmc/ublisk/npc/Dianh.java
@@ -0,0 +1,15 @@
+package com.robinmc.ublisk.npc;
+
+import org.bukkit.entity.Player;
+
+import com.robinmc.ublisk.utils.quest.QuestCharacterClass;
+
+public class Dianh implements QuestCharacterClass {
+
+	@Override
+	public void talk(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/robinmc/ublisk/utils/quest/QuestCharacter.java
+++ src/com/robinmc/ublisk/utils/quest/QuestCharacter.java
@@ -1,6 +1,7 @@
 package com.robinmc.ublisk.utils.quest;
 
 import com.robinmc.ublisk.npc.Arzhur;
+import com.robinmc.ublisk.npc.Dianh;
 import com.robinmc.ublisk.npc.Merek;
 import com.robinmc.ublisk.npc.Rasmus;
 import com.robinmc.ublisk.npc.Ulric;
@@ -11,7 +12,9 @@ public enum QuestCharacter {
 	ULRIC(new Ulric(), "Ulric", "Chaspyr", new NPCLocation(38.5, 67, -26.5, -145, 0)),
 	ARZHUR(new Arzhur(), "Arzhur", "Notch", new NPCLocation(111.5, 68, -103.5, -20, 0)), // TODO Arzhur skin
 	ASHER(null, "Asher", "MrParkerl11", new NPCLocation(449.3, 70, -10.5, 75, 5)), // TODO Asher skin
-	RASMUS(new Rasmus(), "Rasmus", "RobinMC", new NPCLocation(1, 1, 1, 1, 1)); // TODO Rasmus skin TODO Rasmus coordinates
+	RASMUS(new Rasmus(), "Rasmus", "RobinMC", new NPCLocation(1, 1, 1, 1, 1)), // TODO Rasmus skin TODO Rasmus coordinates
+	DIANH(new Dianh(), "Dianh", ":D", new NPCLocation(1, 1, 1, 1, 1));// TODO coordinaten en skin van Dianh
+	
 	private QuestCharacterClass qcc;
 	private String name;
 	private String skin;
