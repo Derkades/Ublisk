91f312d7297ba72b85ccc8a39628514aefb9c5d8
RobinMC <robinslot@hotmail.nl>
2016-08-11 20:39:03 +0200

    Double XP cooldown variable & updated URL

--- src/com/robinmc/ublisk/utils/variable/Var.java
+++ src/com/robinmc/ublisk/utils/variable/Var.java
@@ -7,10 +7,13 @@ public class Var {
 	
 	public static World world = Bukkit.getWorld("ublisk");
 	
-	public static String packURL = "http://ublisk.robinmc.com/textures.zip";
+	public static String packURL = "http://ublisk.robinmc.com/texture/UbliskTextures2.zip";
 	
 	public static int doubleExpTime = 60;
 	
 	public static int xpDivision = 20;
+	
+	//Double XP cooldown in minutes
+	public static int doubleXPCooldown = 10;
 
 }
