09b38a95a531fe8c19165eb49a988ff4e2a84a4e
RobinMC <robinslot@hotmail.nl>
2016-08-12 13:03:32 +0200

    Remove XP orbs when mob is killed

--- src/com/robinmc/ublisk/listeners/EntityDeath.java
+++ src/com/robinmc/ublisk/listeners/EntityDeath.java
@@ -1,5 +1,7 @@
 package com.robinmc.ublisk.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -8,12 +10,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
 
+import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.utils.Exp;
 import com.robinmc.ublisk.utils.enums.Tracker;
 import com.robinmc.ublisk.utils.exception.MobNotFoundException;
 import com.robinmc.ublisk.utils.exception.UnknownAreaException;
 import com.robinmc.ublisk.utils.mob.Mob;
 import com.robinmc.ublisk.utils.variable.Message;
+import com.robinmc.ublisk.utils.variable.Var;
 
 public class EntityDeath implements Listener {
 	
@@ -32,6 +36,15 @@ public class EntityDeath implements Listener {
 				try {
 					Exp.giveMobExp(player, entity);
 					Exp.refresh(player);
+					Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable(){
+						public void run(){
+							for (Entity entity : Var.world.getEntities()){
+								if (entity.getType() == EntityType.EXPERIENCE_ORB){
+									entity.remove();
+								}
+							}
+						}
+					}, 2);
 				} catch (MobNotFoundException | UnknownAreaException e) {
 					player.sendMessage(Message.ERROR_GENERAL.get());
 				}
