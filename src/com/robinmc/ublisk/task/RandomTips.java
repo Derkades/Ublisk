047578343cfdeae9416d0cbe0aa2539c09b3ffd6
RobinMC <robinslot@hotmail.nl>
2016-08-27 17:57:55 +0200

    Display tips in actionbar

--- src/com/robinmc/ublisk/task/RandomTips.java
+++ src/com/robinmc/ublisk/task/RandomTips.java
@@ -3,6 +3,7 @@ package com.robinmc.ublisk.task;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import com.connorlinfoot.actionbarapi.ActionBarAPI;
 import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.enums.Tip;
 import com.robinmc.ublisk.utils.scheduler.Task;
@@ -14,7 +15,8 @@ public class RandomTips implements Task {
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable(){
 			public void run(){
 				for (Player player : Bukkit.getOnlinePlayers()){
-					player.sendMessage(Tip.getRandom());
+					//player.sendMessage(Tip.getRandom());
+					ActionBarAPI.sendActionBar(player, Tip.getRandom(), 100);
 				}
 			}
 		}, 20*20, 5*60*20);
