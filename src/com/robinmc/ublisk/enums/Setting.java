35749bcfbbadc1566308101aba6c3094c2dbd7ce
RobinMC <robinslot@hotmail.nl>
2016-08-29 22:25:57 +0200

    Added more information to settings

--- src/com/robinmc/ublisk/enums/Setting.java
+++ src/com/robinmc/ublisk/enums/Setting.java
@@ -7,18 +7,30 @@ import com.robinmc.ublisk.utils.exception.NotSetException;
 
 public enum Setting {
 	
-	FRIENDS_SHOW_HEALTH("friends-show-health"),
-	PLAY_MUSIC("music"),
-	PM_SOUND("pm-sound");
+	FRIENDS_SHOW_HEALTH("Show friend's health", "friends-show-health", "Shows friend's health in a boss bar."),
+	PLAY_MUSIC("Play music", "music", "Music is different for every town."),
+	PM_SOUND("Play PM sound", "pm-sound", "Play a sound if someone sends you a private message.");
 	
 	private String s;
+	private String name;
+	private String info;
 	
-	Setting(String s){
+	Setting(String name, String s, String info){
 		this.s = s;
+		this.name = name;
+		this.info = info;
 	}
 	
 	public String getString(){
 		return s;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getInfo(){
+		return info;
 	}
 	
 	public boolean get(Player player) throws NotSetException{
