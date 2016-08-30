2056964fd27e8ea1a92be96ab705d6e5bd5b1661
RobinMC <robinslot@hotmail.nl>
2016-08-30 14:15:35 +0200

    Exception for when group can not be found

---
+++ src/com/robinmc/ublisk/utils/exception/GroupNotFoundException.java
@@ -0,0 +1,9 @@
+package com.robinmc.ublisk.utils.exception;
+
+public class GroupNotFoundException extends Exception {
+
+	//TODO: Generate new serialVersionUID
+	private static final long serialVersionUID = 1L;
+
+
+}
