46a12130ec44001df79ef520a52e58dab802b0f5
RobinMC <robinslot@hotmail.nl>
2016-09-01 19:38:23 +0200

    Updated to new logging system

--- src/com/robinmc/ublisk/task/CheckTown.java
+++ src/com/robinmc/ublisk/task/CheckTown.java
@@ -8,7 +8,8 @@ import org.bukkit.entity.Player;
 import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.enums.Town;
 import com.robinmc.ublisk.utils.Config;
-import com.robinmc.ublisk.utils.Console;
+import com.robinmc.ublisk.utils.logging.LogLevel;
+import com.robinmc.ublisk.utils.logging.Logger;
 import com.robinmc.ublisk.utils.scheduler.Task;
 
 public class CheckTown implements Task {
@@ -27,7 +28,7 @@ public class CheckTown implements Task {
 								loc.getZ() > town.moreZ()){
 							if (!(town.getName() == Config.getString("last-town." + player.getUniqueId()))){
 								player.sendTitle("", ChatColor.GRAY + "You are now in " + town.getName());
-								Console.sendMessage("[Towns] " + player.getName() + " is now in " + town.getName() + " and got there from " + Config.getString("last-town." + player.getUniqueId()));
+								Logger.log(LogLevel.INFO, "Town", player.getName() + " is now in " + town.getName() + " and got there from " + Config.getString("last-town." + player.getUniqueId()));
 								Config.set("last-town." + player.getUniqueId(), town.getName());
 							}
 						}
