52ab872219a2eda06b64b8fd6ada8ff5bf791e77
RobinMC <robinslot@hotmail.nl>
2016-09-01 19:35:35 +0200

    Removed try/catch block

--- src/com/robinmc/ublisk/utils/Console.java
+++ src/com/robinmc/ublisk/utils/Console.java
@@ -1,12 +1,6 @@
 package com.robinmc.ublisk.utils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
-import com.robinmc.ublisk.utils.logging.LogLevel;
-import com.robinmc.ublisk.utils.logging.Logger;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class Console {
 	
@@ -15,12 +9,7 @@ public class Console {
 	 * @param cmd The command to be executed
 	 */
 	public static void sendCommand(String cmd){
-		try {
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
-		} catch (Exception e){
-			sendMessage("An error occured while attempting to perform a console command!");
-			sendMessage("Command: " + cmd);
-		}
+		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), cmd);
 	}
 
 }
