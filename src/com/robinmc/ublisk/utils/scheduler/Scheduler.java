9ba513f84cb64e737225f9be0477fd180a15f6d8
RobinMC <robinslot@hotmail.nl>
2016-09-03 17:20:14 +0200

    Scheduler API

---
+++ src/com/robinmc/ublisk/utils/scheduler/Scheduler.java
@@ -0,0 +1,21 @@
+package com.robinmc.ublisk.utils.scheduler;
+
+import org.bukkit.Bukkit;
+
+import com.robinmc.ublisk.Main;
+
+public class Scheduler {
+	
+	public static void runTaskLater(long delay, Runnable runnable){
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), runnable, delay);
+	}
+	
+	public static void runAsyncRepeatingTask(long start, long delay, Runnable runnable){
+		Bukkit.getScheduler().runTaskTimerAsynchronously(Main.getInstance(), runnable, start, delay);
+	}
+	
+	public static void runSyncRepeatingTask(long start, long delay, Runnable runnable){
+		Bukkit.getScheduler().runTaskTimer(Main.getInstance(), runnable, start, delay);
+	}
+
+}
