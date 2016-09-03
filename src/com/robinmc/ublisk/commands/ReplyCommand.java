0eadd451b56db442790abe3d0147457e9b77c273
RobinMC <robinslot@hotmail.nl>
2016-09-03 17:24:11 +0200

    Moved MessageTarget class to .utils.chat

--- src/com/robinmc/ublisk/commands/ReplyCommand.java
+++ src/com/robinmc/ublisk/commands/ReplyCommand.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.robinmc.ublisk.utils.MessageTarget;
+import com.robinmc.ublisk.utils.chat.MessageTarget;
 import com.robinmc.ublisk.utils.variable.Message;
 
 public class ReplyCommand implements CommandExecutor {
