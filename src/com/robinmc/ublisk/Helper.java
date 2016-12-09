0230062f58b0e1af2997db891d2e472744027487
RobinMC <robinslot@hotmail.nl>
2016-12-09 17:12:56 +0100

    Cleaned up the mess that was the Helper enum

--- src/com/robinmc/ublisk/Helper.java
+++ src/com/robinmc/ublisk/Helper.java
@@ -10,19 +10,57 @@ import com.robinmc.ublisk.utils.inventory.InvUtils;
 
 public enum Helper {
 	
-	ROBINMC("RobinMC (Robin)", "https://goo.gl/Aay9Ry", "Plugin development, bug testing, buliding, textures, weapons, quests, music"),
-	CHASPYR("Chaspyr (Casper)", "https://twitter.com/ItsChaspyr", "Building"),
-	GLITCHERDOTBE("GlitcherDOTbe", "http://www.planetminecraft.com/texture_pack/glitchs-3d-addons-pack-19/", "Textures"),
-	ROTTENNUGGET("RottenNugget", "https://www.reddit.com/r/Minecraft/comments/2922fb/3d_models_resource_pack/", "3D models"),
-	PARKER("MrParkerl11", "https://goo.gl/WFW9zm", "Building"),
-	SANDERP("SenpaiSander (Sander)", "https://www.youtube.com/channel/UC7gVEC704565MNzCB6OS3Ew", "Has done a lot of work for Ublisk! Building, quests, weapons, textures"),
-	SYDAN("SirSydan (Sydan)", "https://www.youtube.com/channel/UCFl5VN9R5uEszEoLrYgTOoA", "Music"),
-	XISUMA("xisumavoid", "https://www.youtube.com/user/xisumavoid", "Textures (stained glass, end rods, dark oak wood)"),
-	BREECKO("Breecko (Brecht)", "https://www.youtube.com/channel/UC4Slc9O6dLsia_fWrgN8Q9Q", "Building, being awesome and motivating :D"),
-	SANDER_GAMING("Sander_Gaming (Sander)", "https://www.youtube.com/channel/UCW-jqEMRdjgimFtarWZGGsg", "Building, textures, being the great person he is :)"),
-	LARISSA("LarissaMC (Larissa)", "No link provided", "Building"),
-	JERRIJN("Jerrijn (Jerrijn)", "No link provided", "Ideas (though sometimes too complicated for me to code)"),
-	JARICK("EpicC4Build (Jarick)", "No link provided", "Building");
+	ROBINMC("RobinMC (Robin)", 
+			"https://goo.gl/Aay9Ry", 
+			"Plugin development, bug testing, buliding, textures, weapons, quests, music"),
+	
+	CHASPYR("Chaspyr (Casper)", 
+			"https://twitter.com/ItsChaspyr", 
+			"Building"),
+	
+	GLITCHERDOTBE("GlitcherDOTbe", 
+			"http://www.planetminecraft.com/texture_pack/glitchs-3d-addons-pack-19/", 
+			"Textures"),
+	
+	ROTTENNUGGET("RottenNugget", 
+			"https://www.reddit.com/r/Minecraft/comments/2922fb/3d_models_resource_pack/", 
+			
+			"3D models"),
+	PARKER("MrParkerl11", 
+			"https://goo.gl/WFW9zm", 
+			"Building"),
+	
+	SANDERP("SenpaiSander (Sander)", 
+			"https://www.youtube.com/channel/UC7gVEC704565MNzCB6OS3Ew", 
+			"Has done a lot of work for Ublisk! Building, quests, weapons, textures"),
+	
+	SYDAN("SirSydan (Sydan)", 
+			"https://www.youtube.com/channel/UCFl5VN9R5uEszEoLrYgTOoA", 
+			"Music"),
+	
+	XISUMA("xisumavoid", 
+			"https://www.youtube.com/user/xisumavoid", 
+			"Textures (stained glass, end rods, dark oak wood)"),
+	
+	BREECKO("Breecko (Brecht)", 
+			"https://www.youtube.com/channel/UC4Slc9O6dLsia_fWrgN8Q9Q", 
+			"Building, being awesome and motivating :D"),
+	
+	SANDER_GAMING("Sander_Gaming (Sander)", 
+			"https://www.youtube.com/channel/UCW-jqEMRdjgimFtarWZGGsg", 
+			"Building, textures, being the great person he is :)"),
+	
+	LARISSA("LarissaMC (Larissa)", 
+			"No link provided", 
+			"Building"),
+	
+	JERRIJN("Jerrijn (Jerrijn)", 
+			"No link provided", 
+			"Ideas and inspiration"),
+	
+	JARICK("EpicC4Build (Jarick)", 
+			"No link provided", 
+			"Building");
 	
 	private String ign;
 	private String link;
