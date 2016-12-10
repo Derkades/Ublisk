d94c6cabe329e261235accf0dfe940dd2fcc6e31
RobinMC <robinslot@hotmail.nl>
2016-12-10 18:04:50 +0100

    Removed deprecated classes

--- src/com/robinmc/ublisk/utils/exception/NPCNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.robinmc.ublisk.utils.exception;
-
-@Deprecated
-public class NPCNotFoundException extends Exception {
-
-	private static final long serialVersionUID = 666045391790987854L;
-
-}
