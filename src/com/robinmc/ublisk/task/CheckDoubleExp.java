f1869530715c26377b843da1ff7e54d594376f0a
RobinMC <robinslot@hotmail.nl>
2016-12-11 19:28:44 +0100

    Double XP MySQL fix

--- src/com/robinmc/ublisk/task/CheckDoubleExp.java
+++ src/com/robinmc/ublisk/task/CheckDoubleExp.java
@@ -1,5 +1,6 @@
 package com.robinmc.ublisk.task;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,45 +10,48 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.robinmc.ublisk.Message;
 import com.robinmc.ublisk.utils.Exp;
+import com.robinmc.ublisk.utils.Logger;
+import com.robinmc.ublisk.utils.Logger.LogLevel;
+import com.robinmc.ublisk.utils.Ublisk;
 import com.robinmc.ublisk.utils.scheduler.Scheduler;
-import com.robinmc.ublisk.utils.sql.MySQL;
 
 public class CheckDoubleExp extends BukkitRunnable {
 
 	@Override
 	public void run(){
 		try {
-			MySQL.openConnection();
-			PreparedStatement sql = MySQL.prepareStatement("SELECT state FROM `bonus` WHERE type='doublexp_1';");
-			ResultSet rs = sql.executeQuery();
-			rs.next();
-			boolean doublexp = rs.getBoolean(1);
-			sql.close();
+			Connection connection = null;
+			PreparedStatement sql = null;
+			ResultSet result = null;
+			PreparedStatement sql1 = null;
 			
-			if (doublexp && Exp.DOUBLE_XP_COOLDOWN){
-				Bukkit.broadcastMessage(Message.DOUBLE_XP_COOLDOWN.get());
-				PreparedStatement sql1 = MySQL.prepareStatement("UPDATE bonus SET state=false WHERE type='doublexp_1'");
-				sql1.executeUpdate();
-				sql1.close();
-				return;
-			} else if (doublexp){
-				startCooldown();
-				Exp.DOUBLE_XP_ACTIVE = true;
-				PreparedStatement sql1 = MySQL.prepareStatement("UPDATE bonus SET state=false WHERE type='doublexp_1'");
-				sql1.executeUpdate();
-				sql1.close();
-			}
-
-		} catch (SQLException e){
-			e.printStackTrace();
-		} finally {
 			try {
-				MySQL.closeConnection();
-			} catch (SQLException e) {
-				e.printStackTrace();
+				connection = Ublisk.getNewDatabaseConnection("Check double xp");
+				sql = connection.prepareStatement("SELECT state FROM `bonus` WHERE type='doublexp_1';");
+				result = sql.executeQuery();
+				result.next();
+				boolean doublexp = result.getBoolean(1);
+				
+				if (doublexp && Exp.DOUBLE_XP_COOLDOWN){
+					Bukkit.broadcastMessage(Message.DOUBLE_XP_COOLDOWN.get());
+				} else if (doublexp){
+					startCooldown();
+					Exp.DOUBLE_XP_ACTIVE = true;
+				}
+				
+				sql1 = connection.prepareStatement("UPDATE bonus SET state=false WHERE type='doublexp_1'");
+				sql1.executeUpdate();
+			} catch (SQLException e){
+				throw e;
+			} finally {
+				sql.close();
+				result.close();
+				sql1.close();
+				connection.close();
 			}
+		} catch (SQLException e){
+			Logger.log(LogLevel.SEVERE, "Double XP", "An error occured while checking for double xp in database.");
 		}
-	
 	}
 	
 	public void startCooldown(){
