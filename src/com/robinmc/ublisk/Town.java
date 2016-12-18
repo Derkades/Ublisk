d6bc6637a3ffd1ede25efd69c765e7b1bfa4d9e9
RobinMC <robinslot@hotmail.nl>
2016-12-18 13:40:44 +0100

    Add desert city as town

--- src/com/robinmc/ublisk/Town.java
+++ src/com/robinmc/ublisk/Town.java
@@ -8,7 +8,8 @@ public enum Town {
 	//x < 100 x > 22 z < -10 z > 90
 	INTRODUCTION("Introduction", 100, 22, -10, -90, 69, 67, 5),
 	GLAENOR("Glaenor", 190, 100, 17, -120, 116, 68, -86),
-	RHOCUS("Rhocus", 200, 100, 400, 240, 122, 82, 288);
+	RHOCUS("Rhocus", 200, 100, 400, 240, 122, 82, 288),
+	NO_NAME("NoName", 645, 516, 60, -70, 604, 74, -41); //TODO Name for sand city
 	
 	private String name;
 	
