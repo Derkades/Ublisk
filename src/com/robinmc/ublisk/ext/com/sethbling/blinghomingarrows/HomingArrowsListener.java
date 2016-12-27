12ebf45f7c35d4f2c88d38657e8103720381f8c9
RobinMC <robinslot@hotmail.nl>
2016-12-27 19:37:41 +0100

    Formatting & comment unused fields

--- src/com/robinmc/ublisk/ext/com/sethbling/blinghomingarrows/HomingArrowsListener.java
+++ src/com/robinmc/ublisk/ext/com/sethbling/blinghomingarrows/HomingArrowsListener.java
@@ -11,29 +11,29 @@ import org.bukkit.util.Vector;
 public class HomingArrowsListener implements Listener {
 
 	@EventHandler
-	public void eventArrowFired(EntityShootBowEvent e){
-		if (((e.getEntity() instanceof LivingEntity)) && ((e.getProjectile() instanceof Arrow))){
+	public void eventArrowFired(EntityShootBowEvent e) {
+		if (((e.getEntity() instanceof LivingEntity)) && ((e.getProjectile() instanceof Arrow))) {
 			LivingEntity player = e.getEntity();
-			
+
 			double minAngle = 6.283185307179586D;
-			
+
 			Entity minEntity = null;
 			for (Entity entity : player.getNearbyEntities(64.0D, 64.0D, 64.0D)) {
-				if ((player.hasLineOfSight(entity)) && ((entity instanceof LivingEntity)) && (!entity.isDead())){
+				if ((player.hasLineOfSight(entity)) && ((entity instanceof LivingEntity)) && (!entity.isDead())) {
 					Vector toTarget = entity.getLocation().toVector().clone().subtract(player.getLocation().toVector());
-					
+
 					double angle = e.getProjectile().getVelocity().angle(toTarget);
-					
-					if (angle < minAngle){
+
+					if (angle < minAngle) {
 						minAngle = angle;
 						minEntity = entity;
 					}
 				}
 			}
-			
+
 			if (minEntity != null)
 				new HomingTask((Arrow) e.getProjectile(), (LivingEntity) minEntity);
 		}
 	}
-	
+
 }
