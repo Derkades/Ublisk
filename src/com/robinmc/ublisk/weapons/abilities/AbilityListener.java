a3c8bb073a43383fdfd46d552596e6b706443c4e
RobinMC <robinslot@hotmail.nl>
2016-12-28 12:14:37 +0100

    Fixed ability listener

--- src/com/robinmc/ublisk/weapons/abilities/AbilityListener.java
+++ src/com/robinmc/ublisk/weapons/abilities/AbilityListener.java
@@ -1,28 +1,43 @@
 package com.robinmc.ublisk.weapons.abilities;
 
+import java.util.Set;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.robinmc.ublisk.utils.UPlayer;
+import com.robinmc.ublisk.weapons.Weapon;
+
 public class AbilityListener implements Listener {
 
-	//TODO Fix ability listener
-
 	@EventHandler
-	public void onLeftClick(PlayerInteractEvent event) {
-		if (event.getAction() != Action.LEFT_CLICK_AIR && event.getAction() != Action.LEFT_CLICK_BLOCK) {
+	public void onInteract(PlayerInteractEvent event) {
+		Action action = event.getAction();
+		
+		if (action == Action.PHYSICAL)
 			return;
+		
+		UPlayer player = new UPlayer(event);
+		Set<Weapon> weapons = Weapon.getWeapons();
+		
+		if (action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK) {
+			for (Weapon weapon : weapons){
+				if (Weapon.itemStackIsWeapon(player.getInventory().getItemInMainHand(), weapon)){
+					player.doAbility(weapon.getLeftClickAbility());
+				}
+			}
 		}
-
-	}
-
-	@EventHandler
-	public void onRightClick(PlayerInteractEvent event) {
-		if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
-			return;
+		
+		if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+			for (Weapon weapon : weapons){
+				if (Weapon.itemStackIsWeapon(player.getInventory().getItemInMainHand(), weapon)){
+					player.doAbility(weapon.getRightClickAbility());
+				}
+			}
 		}
-
+		
 	}
 
 }
