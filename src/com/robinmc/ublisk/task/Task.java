b787077f06c4c3ee704bc303c26a349e22ad10d0
RobinMC <robinslot@hotmail.nl>
2017-01-02 12:45:08 +0100

    Super fast mana regeneration (temporarely)

--- src/com/robinmc/ublisk/task/Task.java
+++ src/com/robinmc/ublisk/task/Task.java
@@ -19,7 +19,7 @@ public enum Task {
 	PLAYER_LEVEL_UP(new PlayerLevelUp(), 0, 5*20, false),
 	RANDOM_TIP(new RandomTips(), 30*20, 5*60*20, false),
 	REFRESH_LAST_SEEN_DATE(new RefreshLastSeenDate(), 0, 5*20, false),
-	REGENERATE_HUNGER(new RegenerateHunger(), 5*20, 5*20, false),
+	REGENERATE_HUNGER(new RegenerateHunger(), 0, 10, false),
 	REMOVE_MOBS(new RemoveMobs(), 5*60*20, 15*60*20, false),
 	RESPAWN_NPC(new RespawnNPC(), 5*20, 5*60*20, false),
 	SET_MAX_HEALTH(new SetMaxHealth(), 5*20, 5*20, false),
