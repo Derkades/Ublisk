bdf8822ae15399d42b6b2a52f90b04ae4f98b2c5
RobinMC <robinslot@hotmail.nl>
2017-01-07 15:08:30 +0100

    Increase damage!!

--- src/com/robinmc/ublisk/weapons/sword/wood/WoodenLongSword.java
+++ src/com/robinmc/ublisk/weapons/sword/wood/WoodenLongSword.java
@@ -7,8 +7,8 @@ import com.robinmc.ublisk.weapons.sword.AttackSpeed;
 public class WoodenLongSword extends WoodenSword {
 
 	public WoodenLongSword() {
-		super("Wooden Long Sword", WeaponRarity.COMMON, "Slower than average but quite powerful", AttackSpeed.SLOW,
-				5, -1, -1, new Meteorite());
+		super("Wooden Long Sword", WeaponRarity.COMMON, "Slower than average but quite powerful", AttackSpeed.SLOW, 5,
+				-1, -1, new Meteorite(50));
 	}
 
 }
