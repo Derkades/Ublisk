04a4ba5d34bf51a8414145ff28ced4f62d5f93e5
RobinMC <robinslot@hotmail.nl>
2017-01-07 15:08:18 +0100

    Meteorite: Various ability improvements including fake (and more dramatic) explosion

--- src/com/robinmc/ublisk/weapons/abilities/Meteorite.java
+++ src/com/robinmc/ublisk/weapons/abilities/Meteorite.java
@@ -2,17 +2,25 @@ package com.robinmc.ublisk.weapons.abilities;
 
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.block.BlockFace;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import com.robinmc.ublisk.Main;
+import com.robinmc.ublisk.Var;
+import com.robinmc.ublisk.utils.Logger;
 import com.robinmc.ublisk.utils.UPlayer;
 import com.robinmc.ublisk.utils.Ublisk;
+import com.robinmc.ublisk.utils.Logger.LogLevel;
+import com.robinmc.ublisk.utils.Ublisk.Explosion;
 
 public class Meteorite extends Ability {
 
-	public Meteorite() {
+	private int damage;
+	
+	public Meteorite(int damage) {
 		super(4, 0); // TODO Min level
+		this.damage = damage;
 	}
 
 	@Override
@@ -28,14 +36,20 @@ public class Meteorite extends Ability {
 
 				Vector direction = loc.getDirection().normalize();
 
-				double x, y, z;
+				double x, unsquared, y, z;
 
 				double m;
 
 				m = t / 3.14;
 
 				x = direction.getX() * m;
-				y = Math.sqrt(-1 * Math.pow(t - 33, 3));
+				unsquared = -1 * Math.pow(t - 33, 3);
+				if (unsquared <= 0){
+					Logger.log(LogLevel.WARNING, "Abilities", "Meteorite ability: y unsquared is less than 0 (" + unsquared + ")");
+					this.cancel();
+					return;
+				}
+				y = Math.sqrt(unsquared);
 				z = direction.getZ() * m;
 
 				loc.add(x, y, z);
@@ -48,10 +62,14 @@ public class Meteorite extends Ability {
 				if (loc.getY() < (player.getLocation().getY() + 1.5)) {
 					this.cancel();
 
-					Ublisk.createExplosion(loc, 2.0f);
+					//Ublisk.createExplosion(loc, 2.0f);
+					
+					Location lower = new Location(Var.WORLD, loc.getX(), loc.getY(), loc.getZ());
+					lower.setY(loc.getY() - 0.6);
+					Ublisk.createFakeExplosion(loc.getBlock().getRelative(BlockFace.DOWN).getLocation(), damage, 4, Explosion.BLAST_SMALL, Explosion.FIRE, Explosion.SMOKE);
 					
 					loc.setY(loc.getY() - 1);
-					Ublisk.spawnParticle(Particle.LAVA, loc, 100, 1, 0, 1, 0.5);
+					//Ublisk.spawnParticle(Particle.LAVA, loc, 100, 1, 0, 1, 0.5);
 				}
 
 				loc.subtract(x, y, z);
