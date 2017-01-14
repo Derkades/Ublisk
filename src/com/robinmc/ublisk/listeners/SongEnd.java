7c9204edce8f1f482c97b1745797fe95f2b9c85b
RobinMC <robinslot@hotmail.nl>
2017-01-14 12:03:29 +0100

    Destroy SongPlayer

--- src/com/robinmc/ublisk/listeners/SongEnd.java
+++ src/com/robinmc/ublisk/listeners/SongEnd.java
@@ -10,19 +10,24 @@ import com.robinmc.ublisk.utils.UPlayer;
 import com.robinmc.ublisk.utils.exception.PlayerNotFoundException;
 import com.robinmc.ublisk.utils.settings.Setting;
 import com.xxmicloxx.NoteBlockAPI.SongEndEvent;
+import com.xxmicloxx.NoteBlockAPI.SongPlayer;
 
 public class SongEnd implements Listener {
 	
 	@EventHandler
 	public void musicStopped(SongEndEvent event){
 		try {
-		    for (String playername : event.getSongPlayer().getPlayerList()){
+			SongPlayer songPlayer = event.getSongPlayer();
+			
+		    for (String playername : songPlayer.getPlayerList()){
 		    	UPlayer player = new UPlayer(playername);
 		    	if (player.getSetting(Setting.PLAY_MUSIC)){
 					Town town = player.getTown();
 					town.playThemeToPlayer(player);
 				}
 		    }
+		    
+		    songPlayer.destroy();
 		} catch (PlayerNotFoundException e) {
 			Logger.log(LogLevel.WARNING, "Music", "Tried to play new song but player has already logged out");
 		}
