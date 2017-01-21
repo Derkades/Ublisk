094b00e6866029c0cad66bee75e4dd6de6ad4880
RobinMC <robinslot@hotmail.nl>
2017-01-21 20:18:07 +0100

    /report will now save to text file

--- src/com/robinmc/ublisk/commands/Report.java
+++ src/com/robinmc/ublisk/commands/Report.java
@@ -1,16 +1,31 @@
 package com.robinmc.ublisk.commands;
 
+import java.io.File;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.Message;
+import com.robinmc.ublisk.utils.java.FileUtils;
 
 public class Report implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		sender.sendMessage(Message.REPORT.toString());
+		//sender.sendMessage(Message.REPORT.toString());
+		//return true;
+		
+		if (args.length < 1) {
+			sender.sendMessage(Message.WRONG_USAGE.toString());
+		}
+		
+		File file = new File(Main.getInstance().getDataFolder(), "suggestions.txt");
+		FileUtils.appendStringToFile(file, String.join(" ", args) + "\n\n");
+		
+		sender.sendMessage(String.join(" ", args)); //TODO Fancy message
+		
 		return true;
 	}
 
