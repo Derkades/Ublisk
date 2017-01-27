e658088784ba789d7020548230d0403bccea04c4
RobinMC <robinslot@hotmail.nl>
2017-01-27 22:03:03 +0100

    Added method for getting a List<String> from ResultSet

--- src/com/robinmc/ublisk/utils/java/ListUtils.java
+++ src/com/robinmc/ublisk/utils/java/ListUtils.java
@@ -1,5 +1,8 @@
 package com.robinmc.ublisk.utils.java;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtils {
@@ -12,6 +15,16 @@ public class ListUtils {
 			}
 		}
 		return contains;
+	}
+	
+	public static List<String> getStringListFromResultSet(ResultSet resultSet, String column) throws SQLException {
+		List<String> list = new ArrayList<String>();
+		
+		while (resultSet.next()){
+			list.add(resultSet.getString(column));
+		}
+		
+		return list;
 	}
 
 }
