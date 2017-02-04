947a40cae602c6abcd6e3ceed97643127366290d
RobinMC <robinslot@hotmail.nl>
2017-02-04 17:39:47 +0100

    Added mobtype pig

---
+++ src/com/robinmc/ublisk/mob/type/Pig.java
@@ -0,0 +1,20 @@
+package com.robinmc.ublisk.mob.type;
+
+import org.bukkit.entity.EntityType;
+
+import com.robinmc.ublisk.mob.MobCode;
+import com.robinmc.ublisk.mob.MobType;
+
+public class Pig implements MobType {
+
+	@Override
+	public EntityType getEntityType() {
+		return EntityType.PIG;
+	}
+
+	@Override
+	public MobCode getCode() {
+		return null;
+	}
+
+}
