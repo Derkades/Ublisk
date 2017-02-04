51f9f127aab13fea33ab4fba42b3edd5f97cd059
RobinMC <robinslot@hotmail.nl>
2017-02-04 18:58:21 +0100

    Event priority

--- src/com/robinmc/ublisk/listeners/PlayerQuit.java
+++ src/com/robinmc/ublisk/listeners/PlayerQuit.java
@@ -5,6 +5,7 @@ import static org.bukkit.ChatColor.BOLD;
 import static org.bukkit.ChatColor.DARK_AQUA;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -12,10 +13,12 @@ import com.robinmc.ublisk.utils.UPlayer;
 
 public class PlayerQuit implements Listener {
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onQuit(PlayerQuitEvent event){
 		UPlayer player = new UPlayer(event);
+		
 		event.setQuitMessage(DARK_AQUA + "" + BOLD + player.getName() + AQUA + " has left");
+		
 		player.refreshLastSeenDate();
 	}
 
