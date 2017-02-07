2b909a86865482ad70dec6fb5eceaa229022ff03
RobinMC <robinslot@hotmail.nl>
2017-02-07 16:32:20 +0100

    Sync trackers & player info on quit

--- src/com/robinmc/ublisk/listeners/PlayerQuit.java
+++ src/com/robinmc/ublisk/listeners/PlayerQuit.java
@@ -8,18 +8,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import com.robinmc.ublisk.database.PlayerInfo;
+import com.robinmc.ublisk.database.SyncQueue;
+import com.robinmc.ublisk.database.Tracker;
 import com.robinmc.ublisk.utils.UPlayer;
 
 public class PlayerQuit implements Listener {
 	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onQuit(PlayerQuitEvent event){
-		UPlayer player = new UPlayer(event);
+		final UPlayer player = new UPlayer(event);
 		
 		event.setQuitMessage(DARK_AQUA + "" + BOLD + player.getName() + AQUA + " has left");
 		
 		player.refreshLastSeenDate();
+		
+		SyncQueue.addToQueue(new BukkitRunnable(){
+			public void run(){
+				Tracker.syncWithDatabase(player);
+				PlayerInfo.syncInfo(player);
+			}
+		});
 	}
 
 }
