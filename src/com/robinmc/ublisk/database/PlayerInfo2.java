860f4c787c1ad2ef9ab21b290355cd5c4cbe773f
Robin <robinslot@hotmail.nl>
2017-02-18 21:25:05 +0100

    Remove unused import

--- src/com/robinmc/ublisk/database/PlayerInfo2.java
+++ src/com/robinmc/ublisk/database/PlayerInfo2.java
@@ -2,7 +2,6 @@ package com.robinmc.ublisk.database;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
