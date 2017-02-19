55e3c8e220e3e939e2795d667acf97a398434746
Robin <robinslot@hotmail.nl>
2017-02-19 10:29:12 +0100

    Updated credits :)

--- src/com/robinmc/ublisk/Helper.java
+++ src/com/robinmc/ublisk/Helper.java
@@ -4,28 +4,35 @@ public enum Helper {
 	
 	ROBINMC("RobinMC (Robin)", 
 			"https://goo.gl/Aay9Ry", 
-			"Plugin development, testing, building, textures, weapons, quests, music"),
+			"Plugin development, bug testing, landscaping, a bit of building, textures, quests, music"),
 	
-	CHASPYR("Chaspyr (Casper)", 
-			"https://twitter.com/ItsChaspyr", 
-			"Building, bug testing, textures"),
-	
-	GLITCHERDOTBE("GlitcherDOTbe", 
-			"http://www.planetminecraft.com/texture_pack/glitchs-3d-addons-pack-19/", 
-			"Textures"),
-	
-	ROTTENNUGGET("RottenNugget", 
-			"https://www.reddit.com/r/Minecraft/comments/2922fb/3d_models_resource_pack/", 
-			
-			"3D models"),
-	
-	PARKER("MrParkerl11", 
-			"https://goo.gl/WFW9zm", 
-			"Building"),
+	JARICK("EpicC4Build (Jarick)", 
+			"No link provided", 
+			"Main builder for Rhocus and Dawn Point"),
 	
 	SANDERP("SenpaiSander (Sander)", 
 			"https://www.youtube.com/channel/UC7gVEC704565MNzCB6OS3Ew", 
-			"Has done a lot of work for Ublisk! Building, quests, weapons, textures"),
+			"Building, quests, weapons, textures"),
+	
+	JERRIJN("Jerrijn (Jerrijn)", 
+			"No link provided", 
+			"Custom sword textures, ideas and inspiration"),
+	
+	CHASPYR("Chaspyr (Casper)", 
+			"https://twitter.com/ItsChaspyr", 
+			"Building, bug testing, textures, complaining that he can't play yet"),
+	
+	/*GLITCHERDOTBE("GlitcherDOTbe", 
+			"http://www.planetminecraft.com/texture_pack/glitchs-3d-addons-pack-19/", 
+			"Textures"),*/
+	
+	ROTTENNUGGET("RottenNugget", 
+			"https://www.reddit.com/r/Minecraft/comments/2922fb/3d_models_resource_pack/", 
+			"3D models for some blocks and items"),
+	
+	PARKER("MrParkerl11", 
+			"https://goo.gl/WFW9zm", 
+			"Built one windmill. Yep, that's it."),
 	
 	SYDAN("SirSydan (Sydan)", 
 			"https://www.youtube.com/channel/UCFl5VN9R5uEszEoLrYgTOoA", 
@@ -33,27 +40,21 @@ public enum Helper {
 	
 	XISUMA("xisumavoid", 
 			"https://www.youtube.com/user/xisumavoid", 
-			"Textures (stained glass, end rods, dark oak wood)"),
+			"I used some of his modular resourcepacks in the Ublisk pack."),
 	
 	BREECKO("Breecko (Brecht)", 
 			"https://www.youtube.com/channel/UC4Slc9O6dLsia_fWrgN8Q9Q", 
-			"Building, being awesome and motivating :D"),
+			"A but of building, but mainly being awesome and motivating :D"),
 	
 	SANDER_GAMING("Sander_Gaming (Sander)", 
 			"https://www.youtube.com/channel/UCW-jqEMRdjgimFtarWZGGsg", 
-			"Building, textures, being the great person he is :)"),
+			"Providing me with his great humor while programming"),
 	
 	LARISSA("LarissaMC (Larissa)", 
 			"No link provided", 
 			"Building"),
 	
-	JERRIJN("Jerrijn (Jerrijn)", 
-			"No link provided", 
-			"Ideas and inspiration"),
-	
-	JARICK("EpicC4Build (Jarick)", 
-			"No link provided", 
-			"Building");
+	;
 	
 	private String ign;
 	private String link;
