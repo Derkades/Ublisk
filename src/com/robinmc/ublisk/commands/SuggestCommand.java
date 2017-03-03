3ab6eb442721cec2e6d712f501d3a9ab9abc4d80
RobinMC <robinslot@hotmail.nl>
2017-03-03 22:06:29 +0100

    Cleanup

--- src/com/robinmc/ublisk/commands/SuggestCommand.java
+++ src/com/robinmc/ublisk/commands/SuggestCommand.java
@@ -18,15 +18,9 @@ public class SuggestCommand implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		//sender.sendMessage(Message.REPORT.toString());
-		//return true;
-		
 		if (args.length < 1) {
 			sender.sendMessage(Message.WRONG_USAGE.toString());
 		}
-		
-		//File file = new File(Main.getInstance().getDataFolder(), "suggestions.txt");
-		//FileUtils.appendStringToFile(file, String.join(" ", args) + "\n\n");
 		
 		TodoItem todoItem = new TodoItem(0, "Ublisk", String.join(" ", args));
 		
