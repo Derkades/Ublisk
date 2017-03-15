d329270f369b5b47e866691366b5a2022bbaa9d1
RobinMC <robinslot@hotmail.nl>
2017-03-15 21:33:29 +0100

    Leap ability alleen vanaf de grond

--- src/com/robinmc/ublisk/weapons/abilities/Launch.java
+++ src/com/robinmc/ublisk/weapons/abilities/Launch.java
@@ -5,6 +5,8 @@ import org.bukkit.util.Vector;
 
 import com.robinmc.ublisk.utils.UPlayer;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Launch extends Ability {
 
 	public Launch() {
@@ -13,6 +15,9 @@ public class Launch extends Ability {
 
 	@Override
 	public void run(UPlayer player) {
+		if (!player.onGround())
+			player.sendMessage(ChatColor.RED + "You must be on a solid block to use this ability.");
+		
 		player.setVelocity(player.getLocation().getDirection().multiply(3));
 		player.setVelocity(new Vector(player.getVelocity().getX(), 1.5D, player.getVelocity().getZ()));
 		player.givePotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 3*20, 4);
