f60bf61edd3c624d69ba2d57aa87f8af35343052
RobinMC <robinslot@hotmail.nl>
2017-04-15 19:01:36 +0200

    Cleanup

--- src/com/robinmc/ublisk/weapons/abilities/Ability.java
+++ src/com/robinmc/ublisk/weapons/abilities/Ability.java
@@ -21,37 +21,5 @@ public abstract class Ability {
 	}
 
 	public abstract void run(UPlayer player);
-
-	/*
-	TEST(4, 0, new TestAbility(), new AbilityTrigger(Material.WOOD_SWORD, TriggerType.RIGHT_CLICK)),
-	TEST2(10, 0, new Meteorite(), new AbilityTrigger(Material.GOLD_SWORD, TriggerType.RIGHT_CLICK));*/
-
-	/*
-	private int mana;
-	private int minLevel;
-	private AbilityExecutor exec;
 	
-	Ability(int mana, int minLevel, AbilityExecutor exec){
-		this.mana = mana;
-		this.minLevel = minLevel;
-		this.exec = exec;
-	}
-	
-	public void doAbility(UPlayer player){
-		if (minLevel > player.getLevel()){
-			player.sendMessage(Message.ABILITY_NOT_ENOUGH_LEVEL);
-			return;
-		}
-		
-		try {
-			player.removeMana(mana);
-		} catch (NotEnoughManaException e) {
-			player.sendMessage(Message.ABILITY_NOT_ENOUGH_MANA);
-			return;
-		}
-		
-		exec.doAbility(player);
-	}
-	*/
-
 }
