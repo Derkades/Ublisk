679d4177053e5eef2b03e37eba2a543185ac9dfb
Jerrijn <jerrijn@hotmail.com>
2017-04-22 16:51:34 +0200

    Pew pew ability testing

---
+++ src/com/robinmc/ublisk/weapons/abilities/PewPew.java
@@ -0,0 +1,30 @@
+package com.robinmc.ublisk.weapons.abilities;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.robinmc.ublisk.Main;
+import com.robinmc.ublisk.utils.UPlayer;
+
+public class PewPew extends Ability {
+
+	public PewPew() {
+		super(1, 0);
+	}
+
+	@Override
+	public void run(UPlayer player) {	
+		new BukkitRunnable(){
+			
+			int i = 0;
+			
+			public void run(){
+				player.sendMessage(i);
+				
+				i++;
+				if (i > 5) this.cancel();
+			}
+			
+		}.runTaskTimer(Main.getInstance(), 0, 1*20);	
+	}
+
+}
