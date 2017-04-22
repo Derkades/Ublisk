beb844e1eb5a02d8b9dcd8ae1aef6674108b8442
Jerrijn <jerrijn@hotmail.com>
2017-04-23 01:20:48 +0200

    CastleofStone ability
    Ability om een speler invincible te maken voor een aantal seconden maar
    kan niet bewegen en geen spells casten
    
    Robin help aub ik kan de particles niet werkend krijgen en heb het
    effect nog geen kans gegeven want ik wil eerst de particles

---
+++ src/com/robinmc/ublisk/weapons/abilities/CastleofStone.java
@@ -0,0 +1,36 @@
+package com.robinmc.ublisk.weapons.abilities;
+
+import org.bukkit.Particle;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Location;
+
+import com.robinmc.ublisk.Main;
+import com.robinmc.ublisk.utils.UPlayer;
+import com.robinmc.ublisk.utils.Ublisk;
+import com.robinmc.ublisk.utils.shapes.Direction;
+import com.robinmc.ublisk.utils.shapes.Shapes;
+
+public class CastleofStone extends Ability {
+	
+	public CastleofStone() {
+		super(1, 0);
+	}
+
+	@Override
+	public void run(final UPlayer player) {
+		new BukkitRunnable() {
+
+			double t = 0;
+			org.bukkit.Location loc = player.getLocation();
+			
+			public void run() {
+				t = t + 1;
+				Ublisk.spawnParticle(Particle.DRAGON_BREATH, Shapes.generateCircle(Direction.HORIZONTAL, loc.add(0, 1, 0), 30, 2.0), 1, 0, 0, 0, 0.1);
+				if(t < 60){
+					this.cancel();
+				}
+			}
+			
+		}.runTaskTimer(Main.getInstance(), 0, 1);
+	}
+}
