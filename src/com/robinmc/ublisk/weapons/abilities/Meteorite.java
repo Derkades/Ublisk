ce63b78b382180c87dcef6864fb64403c71ef01a
RobinMC <robinslot@hotmail.nl>
2017-04-24 16:44:10 +0200

    Meteorite ability rewrite

--- src/com/robinmc/ublisk/weapons/abilities/Meteorite.java
+++ src/com/robinmc/ublisk/weapons/abilities/Meteorite.java
@@ -1,17 +1,16 @@
 package com.robinmc.ublisk.weapons.abilities;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Particle;
-import org.bukkit.block.BlockFace;
+import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.Var;
-import com.robinmc.ublisk.utils.Logger;
 import com.robinmc.ublisk.utils.UPlayer;
 import com.robinmc.ublisk.utils.Ublisk;
-import com.robinmc.ublisk.utils.Logger.LogLevel;
 import com.robinmc.ublisk.utils.Ublisk.Explosion;
 
 public class Meteorite extends Ability {
@@ -25,14 +24,43 @@ public class Meteorite extends Ability {
 
 	@Override
 	public void run(final UPlayer player) {
+		Block clickedBlock = player.getTargetBlock(50);
+		if (clickedBlock.getType().equals(Material.AIR)){
+			return;
+		}
+		
+		final Location clickedLocation = clickedBlock.getLocation();
+		
 		new BukkitRunnable() {
 
-			Location loc = player.getLocation();
+			//Location loc = player.getLocation();
 
-			double t = 12;
+			//double t = 12;
+			
+			double y = 100;
 
 			public void run() {
-				t = t + 0.6;
+				
+				if (y <= 2.5){
+					Ublisk.createFakeExplosion(clickedLocation, damage, 4, true, Explosion.BLAST_SMALL, Explosion.FIRE, Explosion.SMOKE);
+					this.cancel();
+					return;
+				}
+				
+				if (y > 50){
+					y -= 5;
+				} else {
+					y -= 2.5;
+				}
+				
+				Location location = new Location(Var.WORLD, clickedLocation.getX(), clickedLocation.getY() + y, clickedLocation.getZ());
+				Ublisk.spawnParticle(Particle.FLAME, location, 5, 0, 1, 0, 0);
+				
+				/*if (location.equals(clickedLocation)){
+					Ublisk.createFakeExplosion(location, damage, 4, true, Explosion.BLAST_SMALL, Explosion.FIRE, Explosion.SMOKE);
+					this.cancel();
+				}*/
+				/*t = t + 0.6;
 
 				Vector direction = loc.getDirection().normalize();
 
@@ -69,7 +97,7 @@ public class Meteorite extends Ability {
 					loc.setY(loc.getY() - 1);
 				}
 
-				loc.subtract(x, y, z);
+				loc.subtract(x, y, z);*/
 			}
 		}.runTaskTimer(Main.getInstance(), 0, 1);
 	}
