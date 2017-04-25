93495c9c608e5602196b47714be63d5dd149d95f
Jerrijn <jerrijn@hotmail.com>
2017-04-26 01:04:33 +0200

    ShieldCircle Particles done + selfnotes
    Selfnotes zijn voor morgen want anders weet ik niet meer wat ik moet
    doen, het is bijna af maar ik had geen tijd meer

--- src/com/robinmc/ublisk/weapons/abilities/ShieldCircle.java
+++ src/com/robinmc/ublisk/weapons/abilities/ShieldCircle.java
@@ -1,10 +1,14 @@
 package com.robinmc.ublisk.weapons.abilities;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.utils.UPlayer;
+import com.robinmc.ublisk.utils.Ublisk;
 
 public class ShieldCircle extends Ability {
 
@@ -23,8 +27,32 @@ public class ShieldCircle extends Ability {
                             double x = t*Math.sin(a);
                             double y = 2*Math.exp(-0.1*t) * Math.sin(t) + 1.5;
                             double z = t*Math.sin(a);
-                            loc.add(x,y,z);
-                            //Ublisk.spawnParticle(particle, location, count, offsetX, offsetY, offsetZ, speed);
+                            loc.add(x,y+1.5,z);
+                            Ublisk.spawnParticle(Particle.REDSTONE, loc, 255, 140, 0, 0);
+                            
+       /*                   for (Entity e : loc.getChunk().getEntities()){ //fix e = entity
+                            	if (e.getLocation().distance(loc) < 1.0){
+                            		if (e.getType() == EntityType.PLAYER){
+                            			new BukkitRunnable(){ //add potion effect saturation
+                            				Location locp = e.getLocation();//fix e = entity
+                            				double b = 0;
+                            				public void run(){
+                            					b += Math.PI/10;
+                            					for (double c = 0; c <= 2*Math.PI; c += Math.PI/40){
+                            						double r = 1.5;
+                            						double x = r*Math.cos(c)*Math.sin(b);
+                            						double y = r*Math.cos(b) + 1;
+                            						double z = r*Math.sin(c)*Math.sin(b);
+                            						loc.add(x, y, z);
+                                                    Ublisk.spawnParticle(Particle.REDSTONE, loc, 255, 140, 0, 0);
+                                                    loc.subtract(x, y, z);
+                            					}
+                            				} // add if cancel
+                            			} //fix ;
+                            		}
+                            	}
+                            }  add runtasktimer  */
+                    
                             loc.subtract(x,y,z);
                            
                             a = a + Math.PI/64;
@@ -33,7 +61,7 @@ public class ShieldCircle extends Ability {
                             y = 2*Math.exp(-0.1*t) * Math.sin(t) + 1.5;
                             z = t*Math.sin(a);
                             loc.add(x,y,z);
-                           // ParticleEffect.WITCH_MAGIC.display(loc,0,0,0,0,1);
+                            Ublisk.spawnParticle(Particle.SPELL_MOB, loc, 255, 0, 0, 0);
                             loc.subtract(x,y,z);
                     }
                     if (t > 20){
