ba09b3bfb491062defd9d46fe2b73c08b51cd22a
Jerrijn <jerrijn@hotmail.com>
2017-04-25 20:08:01 +0200

    Start van ShieldCircle

---
+++ src/com/robinmc/ublisk/weapons/abilities/ShieldCircle.java
@@ -0,0 +1,22 @@
+package com.robinmc.ublisk.weapons.abilities;
+
+import com.robinmc.ublisk.utils.UPlayer;
+
+public class ShieldCircle extends Ability {
+
+	public ShieldCircle() {
+		super(1, 0);
+	}
+	
+	@Override
+	public void run(final UPlayer player) {
+		
+		
+		
+		
+				
+		
+	}
+	
+}
+	
