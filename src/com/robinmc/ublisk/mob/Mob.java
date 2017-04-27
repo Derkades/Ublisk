d07624a94eea3dea7ec140d4f15773f150ba7cf4
RobinMC <robinslot@hotmail.nl>
2017-04-27 08:56:21 +0200

    Revert "Add mob ID"
    This reverts commit 1d280a5cf02cc296515e196b76586812cc01bacd.

--- src/com/robinmc/ublisk/mob/Mob.java
+++ src/com/robinmc/ublisk/mob/Mob.java
@@ -54,40 +54,38 @@ public enum Mob {
 	 *	 Maximum
 	 *
 	 * NOTE: Do not create multiple mobs with the same level and name
-	 * WARNING: DO NOT CHANGE IDs.
 	 */
 	
-	SHEEP_LEVEL_1(1, new MobArea(
+	SHEEP_LEVEL_1(new MobArea(
 			new Radius(27, -50, 5), // Introduction
 			new Radius(29, -45, 2), // Introduction
 			new Radius(135, 85, 10)), // Route from Glaenor to Rhocus
 			new Sheep(), 1, 1.5, 0, 4, 10, "Sheep", 10, GoldDrop.LEVEL1,
 			new MobDrop(new ItemStack(Material.WOOL), 70)),
-	CHICKEN(2, new MobArea(
+	CHICKEN(new MobArea(
 			new Radius(66, -40, 60)), //Introduction
 			new Chicken(), 1, 0.5, 0, 2, 20, "Chicken", 5, GoldDrop.LEVEL1,
 			new MobDrop(new ItemStack(Material.FEATHER), 30)),
-	RUINS_DWELLER(3, new MobArea(
+	RUINS_DWELLER(new MobArea(
 			new Radius(221, -23, 20)), //Ruins near Glaenor forest
 			new ZombieVillager().withProfession(Profession.FARMER), 4, 10, 2, 7, 7, "Ruins Dweller", 10, GoldDrop.LEVEL3,
 			new MobDrop(new ItemStack(Material.ROTTEN_FLESH), 0, 2)),
-	ZOMBIE_LEVEL_3(4, new MobArea(
+	ZOMBIE_LEVEL_3(new MobArea(
 			new Radius(160, 89, 100), //TODO Merge into one big radius
 			new Radius(214, -66, 60),
 			new Radius(111, 19, 50)),
 			new Zombie(), 3, 7, 0, 5, 50, "Zombie", 5, GoldDrop.LEVEL2),
-	RHOCUS_ZOMBIE(5, new MobArea(
+	RHOCUS_ZOMBIE(new MobArea(
 			new Radius(280, 90, 318, 60)),
 			new Zombie(), 15, 15, 5, 15, 50, "Rhocus Zombie", 5, GoldDrop.LEVEL3,
 			new MobDrop(new ItemStack(Material.ROTTEN_FLESH), 0, 2),
 			new MobDrop(new ItemStack(Material.ROTTEN_FLESH), 2, 3)),
-	RHOCUS_PIG(6, new MobArea(
+	RHOCUS_PIG(new MobArea(
 			new Radius(148, 90, 261, 4)),
 			new Pig(), 3, 3.5, 2, 4, 6, "Rhocus Pig", 5, GoldDrop.LEVEL1,
 			new MobDrop(new ItemStack(Material.PORK), 0, 2),
 			new MobDrop(new ItemStack(Material.BONE), 1, 2));
 	
-	private int id;
 	private Radius[] area;
 	private MobType type;
 	private int level;
@@ -100,8 +98,7 @@ public enum Mob {
 	private GoldDrop gold;
 	private MobDrop[] drops;
 
-	Mob(int id, MobArea area, MobType type, int level, double health, int minExp, int maxExp, int maxSpawn, String name, double rate, GoldDrop gold, MobDrop... drops){
-		this.id = id;
+	Mob(MobArea area, MobType type, int level, double health, int minExp, int maxExp, int maxSpawn, String name, double rate, GoldDrop gold, MobDrop... drops){
 		this.area = area.getRadius();
 		this.type = type;
 		this.level = level;
@@ -113,10 +110,6 @@ public enum Mob {
 		this.rate = rate;
 		this.gold = gold;
 		this.drops = drops;
-	}
-	
-	public int getId(){
-		return id;
 	}
 	
 	public Radius[] getAreas(){
