1e4a72f2981ea02447c4837bd5417361e8385d16
RobinMC <robinslot@hotmail.nl>
2017-04-28 15:25:44 +0200

    Weapon API fixes + added Basic Wooden Sword

--- src/com/robinmc/ublisk/weapons/sword/wood/WoodenLongSword.java
+++ src/com/robinmc/ublisk/weapons/sword/wood/WoodenLongSword.java
@@ -1,13 +1,16 @@
 package com.robinmc.ublisk.weapons.sword.wood;
 
+import org.bukkit.Material;
+
 import com.robinmc.ublisk.weapons.WeaponRarity;
 import com.robinmc.ublisk.weapons.abilities.Meteorite;
 import com.robinmc.ublisk.weapons.sword.AttackSpeed;
+import com.robinmc.ublisk.weapons.sword.Sword;
 
-public class WoodenLongSword extends WoodenSword {
+public class WoodenLongSword extends Sword {
 
 	public WoodenLongSword() {
-		super("Wooden Long Sword", WeaponRarity.COMMON, "Slower than average but quite powerful", AttackSpeed.SLOW, 5,
+		super("Wooden Long Sword", Material.GLASS, WeaponRarity.COMMON, "Slower than average but quite powerful", AttackSpeed.SLOW, 5,
 				-1, -1, new Meteorite(50));
 	}
 
--- src/com/robinmc/ublisk/weapons/sword/wood/WoodenShortSword.java
+++ src/com/robinmc/ublisk/weapons/sword/wood/WoodenShortSword.java
@@ -1,13 +1,16 @@
 package com.robinmc.ublisk.weapons.sword.wood;
 
+import org.bukkit.Material;
+
 import com.robinmc.ublisk.weapons.WeaponRarity;
 import com.robinmc.ublisk.weapons.abilities.ShieldCircle;
 import com.robinmc.ublisk.weapons.sword.AttackSpeed;
+import com.robinmc.ublisk.weapons.sword.Sword;
 
-public class WoodenShortSword extends WoodenSword {
+public class WoodenShortSword extends Sword {
 
 	public WoodenShortSword() {
-		super("Wooden Short Sword", WeaponRarity.COMMON, "Insert description here", AttackSpeed.FAST, 3, -1, -1, new ShieldCircle());
+		super("Wooden Short Sword", Material.GLASS, WeaponRarity.COMMON, "Insert description here", AttackSpeed.FAST, 3, -1, -1, new ShieldCircle());
 	}
 
 }
