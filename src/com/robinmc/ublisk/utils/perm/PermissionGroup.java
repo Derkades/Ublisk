ae816d2acac9f307c019cd86018db5a89d542927
RobinMC <robinslot@hotmail.nl>
2017-05-07 20:44:06 +0200

    Permissions inheritance (fix #26 )

--- src/com/robinmc/ublisk/utils/perm/PermissionGroup.java
+++ src/com/robinmc/ublisk/utils/perm/PermissionGroup.java
@@ -1,30 +1,32 @@
 package com.robinmc.ublisk.utils.perm;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public enum PermissionGroup {
 	
-	DEFAULT("Default", "&7&lMember&8:"),
-	BUILDER("Builder", "&c&lBuilder&8:&6&l",
-			Permission.COMMAND_DEBUG,
-			Permission.BUILDER_MODE),
-	MODERATOR("Moderator", "&e&lModerator&8:&6&l",
-			Permission.COMMAND_MUTE,
-			Permission.COMMAND_DEBUG,
-			Permission.COMMANDLOG),
-	OWNER("Owner", "&2&lOwner&8:&6&l", 
-			Permission.COMMAND_DEBUG, 
-			Permission.COMMAND_MUTE, 
-			Permission.COMMANDLOG,
-			Permission.BUILDER_MODE
-			);
+	DEFAULT(null, "Default", "&7&lMember&8:"),
 	
+	MODERATOR(PermissionGroup.DEFAULT, "Moderator", "&e&lModerator&8:&6&l",
+			Permission.COMMAND_MUTE,
+			Permission.COMMAND_DEBUG),
+	
+	BUILDER(PermissionGroup.MODERATOR, "Builder", "&c&lBuilder&8:&6&l",
+			Permission.BUILDER_MODE),
+	
+	HELPER(PermissionGroup.BUILDER, "Helper", "Helper",
+			Permission.COMMAND_DEBUG),
+
+	OWNER(PermissionGroup.MODERATOR, "Owner", "&2&lOwner&8:&6&l");
+	
+	private PermissionGroup parent;
 	private String name;
 	private String prefix;
 	private List<Permission> permissions;
 	
-	PermissionGroup(String name, String prefix, Permission... permissions){
+	PermissionGroup(PermissionGroup parent, String name, String prefix, Permission... permissions){
+		this.parent = parent;
 		this.permissions = Arrays.asList(permissions);
 		this.name = name;
 		this.prefix = prefix;
@@ -39,6 +41,16 @@ public enum PermissionGroup {
 	}
 	
 	public List<Permission> getPermissions(){
+		List<Permission> permissions = new ArrayList<Permission>();
+		PermissionGroup current = this;
+		while (current != null){
+			for (Permission permission : current.permissions){
+				if (!permissions.contains(permission)){
+					permissions.add(permission);
+				}
+			}
+			current = current.parent;
+		}
 		return permissions;
 	}
 	
