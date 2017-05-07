8c45ef8ee2437b11a057a50e840634e69012f388
RobinMC <robinslot@hotmail.nl>
2017-05-07 16:29:47 +0200

    #25

--- src/com/robinmc/ublisk/modules/Groups.java
+++ src/com/robinmc/ublisk/modules/Groups.java
@@ -1,21 +1,25 @@
 package com.robinmc.ublisk.modules;
 
-import static org.bukkit.ChatColor.DARK_GRAY;
-import static org.bukkit.ChatColor.GRAY;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.robinmc.ublisk.HashMaps;
 import com.robinmc.ublisk.utils.UPlayer;
+import com.robinmc.ublisk.utils.Ublisk;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
 
 public class Groups extends UModule {
 	
-	@EventHandler(priority = EventPriority.HIGH)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onChat(AsyncPlayerChatEvent event){
+		event.setCancelled(true); // Don't send chat message, we'll send a custom message
+		
 		UPlayer player = new UPlayer(event);
 		
 		ChatColor chatColor = ChatColor.WHITE;
@@ -23,9 +27,32 @@ public class Groups extends UModule {
 			chatColor = ChatColor.GRAY;
 		
 		int level = player.getLevel();
-		String prefix = player.getGroup().getPrefix();
-		String format = DARK_GRAY + "[" + GRAY + level + DARK_GRAY + "] " + prefix + " %s" + DARK_GRAY + ": " + chatColor + "%s";
-		event.setFormat(format);
+		//String prefix = player.getGroup().getPrefix();
+		//String message = DARK_GRAY + "[" + GRAY + level + DARK_GRAY + "] " + prefix + " " + player.getName() + DARK_GRAY + ": " + chatColor + event.getMessage();
+		
+		BaseComponent[] message = new ComponentBuilder("")
+				.append("[").reset().color(ChatColor.DARK_GRAY)
+				.append(level + "").reset().color(ChatColor.GRAY)
+				.append("] ").reset().color(ChatColor.DARK_GRAY)
+				.append("Prefix")
+				.append(": ").reset().color(ChatColor.DARK_GRAY)
+				.append(" ")
+				.append(player.getName()).reset().event(new HoverEvent(
+						HoverEvent.Action.SHOW_TEXT,
+						new ComponentBuilder("XP: " + player.getXP()).color(ChatColor.AQUA)
+						.append("\n")
+						.append("Guild: " + player.getGuildName()).color(ChatColor.AQUA)
+						.append("\n\n")
+						.append("Click to open statistics").color(ChatColor.GRAY).italic(true)
+						.create()
+						)).event(new ClickEvent(
+								ClickEvent.Action.OPEN_URL, player.getStatsURL()
+							))
+				.append(": ").reset().color(ChatColor.DARK_GRAY).bold(true)
+				.append(event.getMessage()).reset().color(chatColor)
+				.create();
+		
+		Ublisk.getServer().spigot().broadcast(message);		
 	}
 
 }
