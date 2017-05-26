dd06331e8d624869754583165cb69d5367250f63
RobinMC <robinslot@hotmail.nl>
2017-05-26 20:37:51 +0200

    Remove redundant onenable method

--- src/com/robinmc/ublisk/modules/Advancements.java
+++ src/com/robinmc/ublisk/modules/Advancements.java
@@ -2,15 +2,9 @@ package com.robinmc.ublisk.modules;
 
 import org.bukkit.entity.Player;
 
-import com.robinmc.ublisk.Main;
 import com.robinmc.ublisk.utils.Ublisk;
 
 public class Advancements extends UModule {
-	
-	@Override
-	public void onEnable(Main plugin){
-		
-	}
 	
 	public static void grantAdvancement(Player player, Advancement advancement){
 		Ublisk.sendConsoleCommand("advancement grant " + player.getName() + " until " + advancement.toString());
