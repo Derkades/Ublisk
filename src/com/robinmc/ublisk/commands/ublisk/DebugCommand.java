3610e771231a7d8231de6ec2dcc21d7248c94a24
RobinMC <robinslot@hotmail.nl>
2017-06-04 22:48:22 +0200

    Remove old stuff from /u debug + added cached object count

--- src/com/robinmc/ublisk/commands/ublisk/DebugCommand.java
+++ src/com/robinmc/ublisk/commands/ublisk/DebugCommand.java
@@ -1,7 +1,5 @@
 package com.robinmc.ublisk.commands.ublisk;
 
-import org.bukkit.attribute.Attribute;
-
 import com.robinmc.ublisk.utils.UPlayer;
 import com.robinmc.ublisk.utils.caching.Cache;
 import com.robinmc.ublisk.utils.caching.CacheObject;
@@ -12,16 +10,12 @@ public class DebugCommand extends UbliskCommand {
 	protected void onCommand(UPlayer player, String[] args) {
 		//Feel free to delete everything below this to test your own stuff
 		
-		player.sendMessage("Health: " + player.getHealth());
-		player.sendMessage("Health scale: " + player.getPlayer().getHealthScale());
-		player.sendMessage("Max health attribute: " + player.getAttribute(Attribute.GENERIC_MAX_HEALTH));
-		player.sendMessage("Level: " + player.getLevel());
-		player.sendMessage("Correct max health: " + player.getMaxHealth());
-		player.sendMessage("Displayed health (= health / health attribute * health scale): " + player.getHealth() / (player.getAttribute(Attribute.GENERIC_MAX_HEALTH) * player.getPlayer().getHealthScale()));
-	
 		for (CacheObject cache : Cache.CACHE_OBJECT_LIST){
 			player.sendMessage(cache.identifier + " : " + cache.toString() + " : " +  cache.timeout);
 		}
+		
+		player.sendSpacers(3);
+		player.sendMessage(Cache.CACHE_OBJECT_LIST.size());
 	}
 
 	@Override
