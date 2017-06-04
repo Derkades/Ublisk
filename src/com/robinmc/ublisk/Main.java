3284541e51278ac5eaea8027199657819f45ea35
RobinMC <robinslot@hotmail.nl>
2017-06-04 22:27:10 +0200

    Fix #39

--- src/com/robinmc/ublisk/Main.java
+++ src/com/robinmc/ublisk/Main.java
@@ -1,5 +1,6 @@
 package com.robinmc.ublisk;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -80,6 +81,8 @@ public class Main extends JavaPlugin {
 				}
 			}
 		}.runTaskTimer(this, 1*20, 10*20);
+		
+		Bukkit.clearRecipes();
 	}
 	
 	@Override
