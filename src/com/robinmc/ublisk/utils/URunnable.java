aa43cac2370ab4e55ca2e851b7048889a20cc706
RobinMC <robinslot@hotmail.nl>
2017-06-05 17:00:17 +0200

    Improved URunnable

--- src/com/robinmc/ublisk/utils/URunnable.java
+++ src/com/robinmc/ublisk/utils/URunnable.java
@@ -9,52 +9,68 @@ public abstract class URunnable {
 	
 	public abstract void run();
 	
-	public BukkitTask runTimer(long delay, long period) {
-		return new BukkitRunnable(){
+	private BukkitTask task;
+	
+	public void runTimer(long delay, long period) {
+		if (task != null) throw new UnsupportedOperationException("Task has already been scheduled.");
+		
+		task = new BukkitRunnable(){
 			public void run(){
 				URunnable.this.run();
 			}
 		}.runTaskTimer(Main.getInstance(), delay, period);
 	}
 	
-	public BukkitTask runTimer(long period){
-		return runTimer(0, period);
+	public void runTimer(long period){
+		runTimer(0, period);
 	}
 	
-	public BukkitTask runTimerAsync(long delay, long period){
-		return new BukkitRunnable(){
+	public void runTimerAsync(long delay, long period){
+		if (task != null) throw new UnsupportedOperationException("Task has already been scheduled.");
+		
+		task = new BukkitRunnable(){
 			public void run(){
 				URunnable.this.run();
 			}
 		}.runTaskTimerAsynchronously(Main.getInstance(), delay, period);
 	}
 	
-	public BukkitTask runTimerAsync(long period){
-		return runTimerAsync(0, period);
+	public void runTimerAsync(long period){
+		runTimerAsync(0, period);
 	}
 	
-	public BukkitTask runLater(long delay){
-		return new BukkitRunnable(){
+	public void runLater(long delay){
+		if (task != null) throw new UnsupportedOperationException("Task has already been scheduled.");
+		
+		task = new BukkitRunnable(){
 			public void run(){
 				URunnable.this.run();
 			}
 		}.runTaskLater(Main.getInstance(), delay);
 	}
 	
-	public BukkitTask runLaterAsync(long delay){
-		return new BukkitRunnable(){
+	public void runLaterAsync(long delay){
+		if (task != null) throw new UnsupportedOperationException("Task has already been scheduled.");
+		
+		task = new BukkitRunnable(){
 			public void run(){
 				URunnable.this.run();
 			}
 		}.runTaskLaterAsynchronously(Main.getInstance(), delay);
 	}
 	
-	public BukkitTask runAsync(){
-		return new BukkitRunnable(){
+	public void runAsync(){
+		if (task != null) throw new UnsupportedOperationException("Task has already been scheduled.");
+		
+		task = new BukkitRunnable(){
 			public void run(){
 				URunnable.this.run();
 			}
 		}.runTaskAsynchronously(Main.getInstance());
+	}
+	
+	public void cancel(){
+		task.cancel();
 	}
 
 }
