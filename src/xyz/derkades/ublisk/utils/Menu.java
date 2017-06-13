25eb8668178d680507452895f2de0b36924b9893
RobinMC <robinslot@hotmail.nl>
2017-06-13 20:11:02 +0200

    Constructor updates and stuff

--- src/xyz/derkades/ublisk/utils/Menu.java
+++ src/xyz/derkades/ublisk/utils/Menu.java
@@ -1,17 +1,11 @@
 package xyz.derkades.ublisk.utils;
 
-import org.bukkit.entity.Player;
-
 import xyz.derkades.ublisk.Main;
 
 public abstract class Menu extends xyz.derkades.derkutils.bukkit.IconMenu {
 
-	public Menu(String name, int size) {
-		super(Main.getInstance(), name, size);
-	}
-	
-	public Menu(String name, int size, Player player) {
-		super(Main.getInstance(), name, size, player);
+	public Menu(String name, int size, UPlayer player) {
+		super(Main.getInstance(), name, size, player.getPlayer());
 	}
 
 }
