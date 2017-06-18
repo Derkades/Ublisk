d1a4c1bc0ebfd8c298ddf476a415d5528958badf
RobinMC <robinslot@hotmail.nl>
2017-06-18 10:43:22 +0200

    Remove deprecated classes

--- src/xyz/derkades/ublisk/utils/exception/LastSenderUnknownException.java
+++
@@ -1,8 +0,0 @@
-package xyz.derkades.ublisk.utils.exception;
-
-@Deprecated
-public class LastSenderUnknownException extends Exception {
-
-	private static final long serialVersionUID = 4821676267310500210L;
-
-}
--- src/xyz/derkades/ublisk/utils/exception/NotInGuildException.java
+++
@@ -1,8 +0,0 @@
-package xyz.derkades.ublisk.utils.exception;
-
-@Deprecated
-public class NotInGuildException extends Exception {
-
-	private static final long serialVersionUID = 7391375806376278968L;
-
-}
