b90f75429bbb7a2ae566cab13f8f077917eb95aa
RobinMC <robinslot@hotmail.nl>
2017-06-18 10:51:50 +0200

    Explicit class path is no longer necessary

--- src/xyz/derkades/ublisk/utils/Menu.java
+++ src/xyz/derkades/ublisk/utils/Menu.java
@@ -1,8 +1,9 @@
 package xyz.derkades.ublisk.utils;
 
+import xyz.derkades.derkutils.bukkit.IconMenu;
 import xyz.derkades.ublisk.Main;
 
-public abstract class Menu extends xyz.derkades.derkutils.bukkit.IconMenu {
+public abstract class Menu extends IconMenu {
 
 	public Menu(String name, int size, UPlayer player) {
 		super(Main.getInstance(), name, size, player.getPlayer());
