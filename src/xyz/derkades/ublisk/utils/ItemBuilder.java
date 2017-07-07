3b689c5850a034ce8f03bf9bfb078d8ba8da8f9c
RobinMC <robinslot@hotmail.nl>
2017-07-07 22:41:42 +0200

    ADd more constructors

--- src/xyz/derkades/ublisk/utils/ItemBuilder.java
+++ src/xyz/derkades/ublisk/utils/ItemBuilder.java
@@ -1,11 +1,26 @@
 package xyz.derkades.ublisk.utils;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 import xyz.derkades.ublisk.utils.inventory.Item;
 
 public class ItemBuilder extends xyz.derkades.derkutils.bukkit.ItemBuilder {
 	
 	public ItemBuilder(Item item) {
 		super(item.getItemStack());
+	}
+	
+	public ItemBuilder(Material material) {
+		super(material);
+	}
+	
+	public ItemBuilder(ItemStack item) {
+		super(item);
+	}
+	
+	public ItemBuilder(String skullOwner) {
+		super(skullOwner);
 	}
 
 }
