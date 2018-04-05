c628a90df99d9987c76c4308f96125fc7036ab5d
Derkades <robinslot@hotmail.nl>
2018-04-05 17:43:35 +0200

    Textures 59

--- src/xyz/derkades/ublisk/modules/ResourcePack.java
+++ src/xyz/derkades/ublisk/modules/ResourcePack.java
@@ -19,7 +19,7 @@ public class ResourcePack extends UModule implements CommandExecutor {
 	/**
 	 * Direct URL to resource pack zip.
 	 */
-	public static final String RESOURCE_PACK_URL = "http://ut.derkades.xyz/UbliskTextures58.zip";
+	public static final String RESOURCE_PACK_URL = "http://ut.derkades.xyz/UbliskTextures59.zip";
 	
 	@Override
 	public void onEnable(){
